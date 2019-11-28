acd3f434042d0a644d052473d4a68290d121e707
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2019-11-28 14:04:09 +0000

    Corrected typo

--- src/main/java/uk/gov/justice/probation/courtcaseservice/controller/PingEndpoint.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/controller/PingEndpoint.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingController {
+public class PingEndpoint {
 
     @RequestMapping(value = "/ping")
     public String ping() {

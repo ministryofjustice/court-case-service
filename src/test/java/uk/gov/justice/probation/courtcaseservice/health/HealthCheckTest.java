20fe82e0b82bc7851b7d6a26505531acb0da539c
katie-cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2019-12-04 08:30:41 +0000

    Pic 177 add postgres (#5)
    * added postgres config to application
    
    * removed typo
    
    * Corrected typo
    
    * added health check property that displays more info about app

---
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/health/HealthCheckTest.java
@@ -0,0 +1,45 @@
+package uk.gov.justice.probation.courtcaseservice.health;
+
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
+public class HealthCheckTest {
+
+    @LocalServerPort
+    int port;
+
+    @Before
+    public void before() {
+        RestAssured.port = port;
+        RestAssured.basePath = "/health";
+    }
+
+    @Test
+    public void testUp() throws IOException {
+
+        String response = given()
+                .when()
+                .get("/")
+                .then()
+                .statusCode(200)
+                .extract().response().asString();
+
+        assertThatJson(response).node("status").isEqualTo("UP");
+        assertThatJson(response).node("details.db.details.database").isEqualTo("PostgreSQL");
+    }
+}

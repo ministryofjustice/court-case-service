c5e1dd9fddf8875774b1214210c10e34d7fbf509
katie-cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2019-12-10 10:37:06 +0000

    Pic 182 add flyway (#9)
    * added postgres config to application
    
    * removed typo
    
    * Corrected typo
    
    * added health check property that displays more info about app
    
    * removed unrequired environment variable
    
    * Updated environment variables to reflect secrets in cloud platform
    
    * removed environment variable
    
    * Added configuration for flyway database migration
    
    * Removed unnecessary method
    
    * Removed unnecessary method

---
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/CourtCasePropertiesEntity.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.probation.courtcaseservice.jpa.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@Data
+@AllArgsConstructor
+public class CourtCasePropertiesEntity implements Serializable {
+
+
+}
---
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/repository/CourtCaseRepository.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.probation.courtcaseservice.jpa.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import uk.gov.justice.probation.courtcaseservice.jpa.entity.CourtCaseEntity;
+
+import java.util.Optional;
+
+@Repository
+public interface CourtCaseRepository extends CrudRepository<CourtCaseEntity, Long> {
+
+
+}

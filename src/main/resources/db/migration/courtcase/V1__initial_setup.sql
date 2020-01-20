7f617ad4efe6b9e4afc06aecc4b38b56583cf107
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2020-01-20 16:01:06 +0000

    PIC-217: amended configuration for flyway

--- src/main/resources/db/migration/courtcase/V1__initial_setup.sql
+++ src/main/resources/db/migration/courtcase/V1__initial_setup.sql
@@ -13,14 +13,16 @@ DROP TABLE IF EXISTS COURT_CASE;
 CREATE TABLE IF NOT EXISTS COURT_CASE
 (
   ID                                     SERIAL       PRIMARY KEY,
-  CASE_ID                                BIGINT       NOT NULL,
+  CASE_ID                                TEXT         NOT NULL,
   CASE_NO                                TEXT         NOT NULL,
-  COURT_ID                               BIGINT       NOT NULL,
+  COURT_CODE                             TEXT         DEFAULT 'SHF' NOT NULL,
   COURT_ROOM                             TEXT         NULL,
   SESSION_START_TIME                     TIMESTAMP    NOT NULL,
+  PROBATION_STATUS                       TEXT         NOT NULL,
   DATA                                   JSONB        NOT NULL,
   CONSTRAINT court_case_case_id_idempotent UNIQUE (CASE_ID),
-  CONSTRAINT fk_court_case_court FOREIGN KEY (COURT_ID) REFERENCES COURT (ID)
+  CONSTRAINT fk_court_case_court FOREIGN KEY (COURT_CODE) REFERENCES COURT (COURT_CODE),
+  CONSTRAINT court_case_case_no_idempotent UNIQUE (CASE_NO)
 );
 
 

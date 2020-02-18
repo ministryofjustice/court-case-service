86e77a453c8fcee57544145ab9edeb4304dc19af
John Evans <John.Evans@digital.homeoffice.gov.uk>
2020-02-18 15:30:56 +0000

    Pic 251 community api auth (#28)
    * Remove prototype code and reconfigure Spring Security
    
    * Configure Spring Security to get token from auth server for test request and update Spring Boot version
    
    * Delegate community API auth function to Spring Security
    
    * Move common config to application.yml and disable endpoint in non-local envs pending valid urls and secrets to deploy to envs
    
    * Disable community-api-test-endpoint in non-local envs

---
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/controller/TokenResponse.java
@@ -0,0 +1,10 @@
+package uk.gov.justice.probation.courtcaseservice.controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class TokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+}
--- src/test/java/uk/gov/justice/probation/courtcaseservice/controller/PingControllerIntTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/controller/PingControllerIntTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static io.restassured.RestAssured.given;
@@ -15,7 +15,7 @@ import static uk.gov.justice.probation.courtcaseservice.TestConfig.configureRest
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringJUnit4ClassRunner.class)
-@DirtiesContext
+@ActiveProfiles(profiles = "test")
 public class PingControllerIntTest {
 
     @LocalServerPort

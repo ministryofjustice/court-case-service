a3b1265dac304911514cfaa7dec2ce4f6fc85edc
John Evans <John.Evans@digital.homeoffice.gov.uk>
2020-02-20 13:42:42 +0000

    Remove redundant migrations which apparently haven't been applied to … (#31)
    * Remove redundant migrations which apparently haven't been applied to envs
    
    * Reintroduce court into seed data

--- src/main/resources/db/migration/courtcase/V100__remove_seed_data.sql
+++
@@ -1,9 +0,0 @@
--- This migration undoes most of the effects (with the exception of adding the SHF court as this is needed) of
--- V99__seed_data.sql which was inadvertently applied to prod.
-
-DELETE FROM court_case WHERE case_id='1168460';
-DELETE FROM court_case WHERE case_id='1246257';
-DELETE FROM court_case WHERE case_id='1246258';
-DELETE FROM court_case WHERE case_id='1246273';
-DELETE FROM court_case WHERE case_id='1248278';
-

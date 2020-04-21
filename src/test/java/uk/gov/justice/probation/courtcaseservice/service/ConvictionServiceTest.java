8906fe8e77847c987fb58d31faff8d3bf5d22401
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2020-04-21 09:19:25 +0100

    395 - Added unpaid work to single conviction call (#69)
    * 395 - Added unpaid work to single conviction call
    
    * 395 - Added unpaid work to single conviction call
    
    * 395 - Added wiremock test fix
    
    * 395 - fix notes

--- src/test/java/uk/gov/justice/probation/courtcaseservice/service/ConvictionServiceTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/service/ConvictionServiceTest.java
@@ -6,6 +6,9 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,9 +16,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
-import uk.gov.justice.probation.courtcaseservice.controller.model.AttendancesResponse;
+import uk.gov.justice.probation.courtcaseservice.controller.model.AttendanceResponse;
+import uk.gov.justice.probation.courtcaseservice.controller.model.ConvictionResponse;
 import uk.gov.justice.probation.courtcaseservice.restclient.ConvictionRestClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.exception.OffenderNotFoundException;
+import uk.gov.justice.probation.courtcaseservice.service.model.Conviction;
+import uk.gov.justice.probation.courtcaseservice.service.model.Sentence;
+import uk.gov.justice.probation.courtcaseservice.service.model.UnpaidWork;
 
 @ExtendWith(MockitoExtension.class)
 class ConvictionServiceTest {
@@ -23,8 +30,10 @@ class ConvictionServiceTest {
     public static final String CRN = "X370652";
     public static final Long SOME_CONVICTION_ID = 1234L;
 
+    private Conviction conviction;
+
     @Mock
-    private AttendancesResponse attendancesResponse;
+    private List<AttendanceResponse> attendancesResponse;
 
     @Mock
     private ConvictionRestClient restClient;
@@ -32,27 +41,70 @@ class ConvictionServiceTest {
     @InjectMocks
     private ConvictionService service;
 
-    @DisplayName("Normal retrieval of attendances")
+    @BeforeEach
+    void beforeEach() {
+        final Sentence sentence = Sentence.builder().unpaidWork(UnpaidWork.builder().acceptableAbsences(100).build()).build();
+        conviction = Conviction.builder().convictionId(String.valueOf(SOME_CONVICTION_ID)).sentence(sentence).build();
+    }
+
+    @DisplayName("Normal retrieval of conviction with unpaid work")
     @Test
-    public void whenGetAttendances_returnAttendances() {
+    public void givenConviction_whenGetConviction_returnConviction() {
 
-        when(restClient.getAttendancesByCrnAndConvictionId(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.just(attendancesResponse));
+        when(restClient.getAttendances(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.just(attendancesResponse));
+        when(restClient.getConviction(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.just(conviction));
 
-        final AttendancesResponse response = service.getAttendances(CRN, SOME_CONVICTION_ID);
+        final ConvictionResponse response = service.getConviction(CRN, SOME_CONVICTION_ID);
 
-        assertThat(response).isSameAs(attendancesResponse);
-        verify(restClient).getAttendancesByCrnAndConvictionId(CRN, SOME_CONVICTION_ID);
+        assertThat(response.getAttendances()).isSameAs(attendancesResponse);
+        assertThat(response.getUnpaidWork().getAcceptableAbsences()).isEqualTo(100);
+        verify(restClient).getAttendances(CRN, SOME_CONVICTION_ID);
+        verify(restClient).getConviction(CRN, SOME_CONVICTION_ID);
         verifyNoMoreInteractions(restClient);
     }
 
-    @DisplayName("An empty response means that we get an exception")
+    @DisplayName("Retrieval of conviction with null sentence and therefore no unpaid work")
+    @Test
+    public void givenNullSentenceOnConviction_whenGetConviction_returnConvictionNoUnPaidWork() {
+
+        conviction = Conviction.builder().convictionId(String.valueOf(SOME_CONVICTION_ID)).build();
+        when(restClient.getAttendances(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.just(attendancesResponse));
+        when(restClient.getConviction(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.just(conviction));
+
+        final ConvictionResponse response = service.getConviction(CRN, SOME_CONVICTION_ID);
+
+        assertThat(response.getAttendances()).isSameAs(attendancesResponse);
+        assertThat(response.getUnpaidWork()).isNull();
+        verify(restClient).getAttendances(CRN, SOME_CONVICTION_ID);
+        verify(restClient).getConviction(CRN, SOME_CONVICTION_ID);
+        verifyNoMoreInteractions(restClient);
+    }
+
+    @DisplayName("Retrieval of conviction with empty list of attendances")
+    @Test
+    public void givenNoAttendances_whenGetConviction_returnConviction() {
+
+        when(restClient.getAttendances(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.just(Collections.emptyList()));
+        when(restClient.getConviction(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.just(conviction));
+
+        final ConvictionResponse response = service.getConviction(CRN, SOME_CONVICTION_ID);
+
+        assertThat(response.getAttendances()).isEmpty();
+        assertThat(response.getUnpaidWork().getAcceptableAbsences()).isEqualTo(100);
+        verify(restClient).getAttendances(CRN, SOME_CONVICTION_ID);
+        verify(restClient).getConviction(CRN, SOME_CONVICTION_ID);
+        verifyNoMoreInteractions(restClient);
+    }
+
+    @DisplayName("An error response means that we get an exception")
     @Test
     void givenAttendancesNotFound_whenGetAttendances_thenThrowException() {
 
-        when(restClient.getAttendancesByCrnAndConvictionId(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.empty());
+        when(restClient.getAttendances(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.error(new OffenderNotFoundException(CRN)));
+        when(restClient.getConviction(CRN, SOME_CONVICTION_ID)).thenReturn(Mono.error(new OffenderNotFoundException(CRN)));
 
         assertThatExceptionOfType(OffenderNotFoundException.class)
-            .isThrownBy(() -> service.getAttendances(CRN, SOME_CONVICTION_ID))
+            .isThrownBy(() -> service.getConviction(CRN, SOME_CONVICTION_ID))
             .withMessageContaining(CRN);
     }
 }

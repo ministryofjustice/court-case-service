dd7f609d06dcf03c0861684adcade9f37f4185d6
John Evans <john.evans@digital.justice.gov.uk>
2020-05-21 10:05:35 +0100

    PIC-387: Fix - Throw exception on call to breach endpoint when NSI returned from community api is not a breach (#84)

--- src/main/java/uk/gov/justice/probation/courtcaseservice/restclient/exception/NsiNotFoundException.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/restclient/exception/NsiNotFoundException.java
@@ -4,4 +4,8 @@ public class NsiNotFoundException extends RestResourceNotFoundException {
     public NsiNotFoundException(String crn, Long convictionId, Long nsiId) {
         super(String.format("Nsi with id '%s' not found for convictionId '%s' and crn '%s'", nsiId, convictionId, crn));
     }
+
+    public NsiNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/BreachService.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/BreachService.java
@@ -1,12 +1,17 @@
 package uk.gov.justice.probation.courtcaseservice.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import uk.gov.justice.probation.courtcaseservice.controller.model.BreachResponse;
 import uk.gov.justice.probation.courtcaseservice.restclient.ConvictionRestClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.NsiRestClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.communityapi.mapper.NsiMapper;
+import uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model.CommunityApiNsi;
+import uk.gov.justice.probation.courtcaseservice.restclient.exception.NsiNotFoundException;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -15,13 +20,24 @@ public class BreachService {
     private final NsiRestClient nsiRestClient;
     private final ConvictionRestClient convictionRestClient;
     private final NsiMapper breachMapper;
+    @Value("#{'${community-api.nsis-filter.codes.breaches}'.split(',')}")
+    private List<String> nsiBreachCodes;
 
     public BreachResponse getBreach(String crn, Long convictionId, Long breachId) {
         return Mono.zip(
                 nsiRestClient.getNsiById(crn, convictionId, breachId),
                 convictionRestClient.getConviction(crn, convictionId)
         )
-                .map(tuple -> breachMapper.breachOf(tuple.getT1(), tuple.getT2()))
+                .map(tuple -> {
+                    CommunityApiNsi nsi = tuple.getT1();
+                    validateBreach(nsi);
+                    return breachMapper.breachOf(nsi, tuple.getT2());
+                })
                 .block();
+    }
+
+    private void validateBreach(CommunityApiNsi nsi) {
+        if (!nsiBreachCodes.contains(nsi.getType().getCode()))
+            throw new NsiNotFoundException(String.format("Breach with id '%s' does not exist", nsi.getNsiId()));
     }
 }

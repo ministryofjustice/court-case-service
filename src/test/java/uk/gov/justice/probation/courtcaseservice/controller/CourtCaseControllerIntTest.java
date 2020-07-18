42ab0b244ea2cd63be84bf8989131c7b7209c5a0
John Evans <john.evans@digital.justice.gov.uk>
2020-07-18 12:26:22 +0100

    ✅️ Generate JWT tokens for integration tests

--- src/test/java/uk/gov/justice/probation/courtcaseservice/controller/CourtCaseControllerIntTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/controller/CourtCaseControllerIntTest.java
@@ -1,5 +1,53 @@
 package uk.gov.justice.probation.courtcaseservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.justice.probation.courtcaseservice.TestConfig;
+import uk.gov.justice.probation.courtcaseservice.jpa.repository.CourtCaseRepository;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.RSAMultiPrimePrivateCrtKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.Date;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,24 +57,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.format.DateTimeFormatter;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.justice.probation.courtcaseservice.TestConfig;
-import uk.gov.justice.probation.courtcaseservice.jpa.repository.CourtCaseRepository;
+import static uk.gov.justice.probation.courtcaseservice.TestConfig.WIREMOCK_PORT;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
@@ -34,8 +65,8 @@ import uk.gov.justice.probation.courtcaseservice.jpa.repository.CourtCaseReposit
 @Sql(scripts = "classpath:before-test.sql", config = @SqlConfig(transactionMode = ISOLATED))
 @Sql(scripts = "classpath:after-test.sql", config = @SqlConfig(transactionMode = ISOLATED), executionPhase = AFTER_TEST_METHOD)
 public class CourtCaseControllerIntTest {
-
-    /* before-test.sql sets up a court case in the database */
+    public static final String KEY_ID = "mock-key";
+    private String token;
 
     @LocalServerPort
     private int port;
@@ -53,15 +84,99 @@ public class CourtCaseControllerIntTest {
     private static final String DEFENDANT_NAME = "JTEST";
     private final LocalDateTime now = LocalDateTime.now();
 
+    @ClassRule
+    public static final WireMockClassRule wireMockRule = new WireMockClassRule(wireMockConfig()
+            .port(WIREMOCK_PORT)
+            .usingFilesUnderClasspath("mocks"));
+
+    @Rule
+    public WireMockClassRule instanceRule = wireMockRule;
+
     @Before
-    public void setup() {
+    public void setup() throws InvalidKeySpecException, NoSuchAlgorithmException, ParseException, JOSEException {
         TestConfig.configureRestAssuredForIntTest(port);
+        token = createToken();
+    }
+
+    private String createToken() throws InvalidKeySpecException, NoSuchAlgorithmException, JOSEException, ParseException {
+        RSAMultiPrimePrivateCrtKeySpec privateCrtKeySpec;
+        RSAKey jwk;
+        try {
+
+            JWKSet localKeys = JWKSet.load(new ClassPathResource("mocks/test_keypair_jwks.json").getFile());
+            jwk = (RSAKey) localKeys.getKeyByKeyId(KEY_ID);
+
+            BigInteger publicKeyModulus = jwk.getModulus().decodeToBigInteger();
+            BigInteger publicExponent = jwk.getPublicExponent().decodeToBigInteger();
+            BigInteger privateExponent = jwk.getPrivateExponent().decodeToBigInteger();
+            BigInteger primeP = jwk.getFirstPrimeFactor().decodeToBigInteger();
+            BigInteger primeQ = jwk.getSecondPrimeFactor().decodeToBigInteger();
+            BigInteger primeExponentP = jwk.getFirstFactorCRTExponent().decodeToBigInteger();
+            BigInteger primeExponentQ = jwk.getSecondFactorCRTExponent().decodeToBigInteger();
+            BigInteger crtCoefficient = jwk.getSecondFactorCRTExponent().decodeToBigInteger();
+
+            privateCrtKeySpec = new RSAMultiPrimePrivateCrtKeySpec(
+                    publicKeyModulus,
+                    publicExponent,
+                    privateExponent,
+                    primeP,
+                    primeQ,
+                    primeExponentP,
+                    primeExponentQ,
+                    crtCoefficient,
+                    null
+            );
+
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+        KeySpec keySpec = privateCrtKeySpec;
+
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(jwk.getModulus().decodeToBigInteger(), jwk.getPublicExponent().decodeToBigInteger());
+        RSAPublicKey rsaPublicJWK = (RSAPublicKey) kf.generatePublic(rsaPublicKeySpec);
+        PrivateKey rsaJWK = kf.generatePrivate(keySpec);
+
+// Create RSA-signer with the private key
+        JWSSigner signer = new RSASSASigner(rsaJWK);
+
+// Prepare JWT with claims set
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("TEST.USER")
+                .issuer("http://localhost:8090/auth/issuer")
+                .claim("authorities", Collections.singletonList("ROLE_PREPARE_A_CASE"))
+                .expirationTime(new Date(new Date().getTime() + 60 * 1000))
+                .build();
+
+        SignedJWT signedJWT = new SignedJWT(
+                new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(KEY_ID).build(),
+                claimsSet);
+
+// Compute the RSA signature
+        signedJWT.sign(signer);
+
+// To serialize to compact form, produces something like
+// eyJhbGciOiJSUzI1NiJ9.SW4gUlNBIHdlIHRydXN0IQ.IRMQENi4nJyp4er2L
+// mZq3ivwoAjqa1uUkSBKFIX7ATndFF5ivnt-m8uApHO4kfIFOrW7w2Ezmlg3Qd
+// maXlS9DhN0nUk_hGI3amEjkKd0BWYCB8vfUbUv0XGjQip78AI4z1PrFRNidm7
+// -jPDm5Iq0SZnjKjCNS5Q15fokXZc8u0A
+        String s = signedJWT.serialize();
+
+// On the consumer side, parse the JWS and verify its RSA signature
+        signedJWT = SignedJWT.parse(s);
+
+        JWSVerifier verifier = new RSASSAVerifier(rsaPublicJWK);
+        assertThat(signedJWT.verify(verifier)).isTrue();
+        return signedJWT.serialize();
     }
 
     @Test
     public void cases_shouldGetCaseListWhenCasesExist() {
 
-        when()
+        given()
+                .auth()
+                .oauth2(token)
+                .when()
                 .get("/court/{courtCode}/cases?date={date}", COURT_CODE, LocalDate.of(2019, 12, 14).format(DateTimeFormatter.ISO_DATE))
                 .then()
                 .assertThat()
@@ -128,7 +243,7 @@ public class CourtCaseControllerIntTest {
     @Test
     public void shouldGetCaseWhenExists() {
         String startTime = LocalDateTime.of(2019, Month.DECEMBER, 14, 9, 0, 0)
-            .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+                .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         given()
                 .when()
                 .header("Accept", "application/json")

6add9b0e08d4b2b7c7d5770bbd70361960d2f6a9
John Evans <john.evans@digital.justice.gov.uk>
2020-07-30 09:21:44 +0100

    🔧 Add configurable web client timeouts

--- src/main/java/uk/gov/justice/probation/courtcaseservice/application/WebClientConfig.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/application/WebClientConfig.java
@@ -1,15 +1,22 @@
 package uk.gov.justice.probation.courtcaseservice.application;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.RestClientHelper;
+
+import java.util.concurrent.TimeUnit;
 
 @EnableJpaAuditing
 @Configuration
@@ -25,6 +32,15 @@ public class WebClientConfig {
 
     @Value("${nomis-oauth.base-url}")
     private String oauthApiBaseUrl;
+
+    @Value("${web.client.connect-timeout-ms}")
+    private Integer connectTimeoutMs;
+
+    @Value("${web.client.read-timeout-ms}")
+    private long readTimeoutMs;
+
+    @Value("${web.client.write-timeout-ms}")
+    private long writeTimeoutMs;
 
     @Bean
     public RestClientHelper communityApiClient(WebClient communityWebClient) {
@@ -54,8 +70,16 @@ public class WebClientConfig {
         ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
                 new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
 
+        HttpClient httpClient = HttpClient.create()
+                .tcpConfiguration(client ->
+                        client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeoutMs)
+                                .doOnConnected(conn -> conn
+                                        .addHandlerLast(new ReadTimeoutHandler(readTimeoutMs, TimeUnit.MILLISECONDS))
+                                        .addHandlerLast(new WriteTimeoutHandler(writeTimeoutMs, TimeUnit.MILLISECONDS))));
+
         return WebClient
                 .builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl(baseUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .apply(oauth2Client.oauth2Configuration())

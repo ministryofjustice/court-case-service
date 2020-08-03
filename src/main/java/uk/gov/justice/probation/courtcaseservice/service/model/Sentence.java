fadd9d633ec9654eca62d884c90098a8919a36c9
John Evans <john.evans@digital.justice.gov.uk>
2020-08-03 14:17:13 +0100

    ✨ Add sentenceId to probationRecord response

--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/model/Sentence.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/model/Sentence.java
@@ -3,12 +3,13 @@ package uk.gov.justice.probation.courtcaseservice.service.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
-import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @ApiModel("Sentence")
 @Data
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Sentence {
+    private final String sentenceId;
     private final String description;
     private final Integer length;
     private final String lengthUnits;

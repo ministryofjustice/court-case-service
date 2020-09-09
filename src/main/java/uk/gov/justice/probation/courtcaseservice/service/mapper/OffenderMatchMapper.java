5c2c1406e17959042db671f9516cdbecce134070
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2020-09-09 14:52:51 +0100

    Pic 727 fix duplicate matches (#184)
    * 🐛 Prevent duplicate group offender matches storage
    
    * 🐛 Prevent duplicate group offender matches storage
    
    * 🐛 Fix save of matches
    
    * 🐛 Remove commented code
    
    Co-authored-by: John Evans <john.evans@digital.justice.gov.uk>

--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/mapper/OffenderMatchMapper.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/mapper/OffenderMatchMapper.java
@@ -1,16 +1,15 @@
 package uk.gov.justice.probation.courtcaseservice.service.mapper;
 
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.probation.courtcaseservice.controller.model.GroupedOffenderMatchesRequest;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.CourtCaseEntity;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.GroupedOffenderMatchesEntity;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.OffenderMatchEntity;
 
-import java.util.stream.Collectors;
-
 @Component
 public class OffenderMatchMapper {
-    public GroupedOffenderMatchesEntity groupedMatchesOf(GroupedOffenderMatchesRequest offenderMatches, CourtCaseEntity courtCase) {
+    public GroupedOffenderMatchesEntity newGroupedMatchesOf(GroupedOffenderMatchesRequest offenderMatches, CourtCaseEntity courtCase) {
         var group = GroupedOffenderMatchesEntity.builder()
                 .courtCase(courtCase)
                 .build();
@@ -28,6 +27,27 @@ public class OffenderMatchMapper {
                                                 .cro(offenderMatchRequest.getMatchIdentifiers().getCro())
                                                 .build()
                 ).collect(Collectors.toList()));
+        return group;
+    }
+
+    public GroupedOffenderMatchesEntity update(GroupedOffenderMatchesEntity group, GroupedOffenderMatchesRequest request) {
+
+        group.clearOffenderMatches();
+
+        request.getMatches().stream()
+            .map(
+                offenderMatchRequest ->
+                    OffenderMatchEntity.builder()
+                        .group(group)
+                        .confirmed(offenderMatchRequest.getConfirmed())
+                        .rejected(offenderMatchRequest.getRejected())
+                        .matchType(offenderMatchRequest.getMatchType())
+                        .crn(offenderMatchRequest.getMatchIdentifiers().getCrn())
+                        .pnc(offenderMatchRequest.getMatchIdentifiers().getPnc())
+                        .cro(offenderMatchRequest.getMatchIdentifiers().getCro())
+                        .build()
+            )
+            .forEach(newMatch -> {group.getOffenderMatches().add(newMatch);});
         return group;
     }
 }

f3811ba9363029362a35f4a638cad3100c2d02c9
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-10-08 15:47:07 +0100

    ✨ pic-803 added name and defendant type

--- src/test/java/uk/gov/justice/probation/courtcaseservice/controller/CourtCaseControllerPutIntTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/controller/CourtCaseControllerPutIntTest.java
@@ -1,5 +1,13 @@
 package uk.gov.justice.probation.courtcaseservice.controller;
 
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
 import org.junit.Before;
@@ -16,15 +24,6 @@ import uk.gov.justice.probation.courtcaseservice.BaseIntTest;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.AddressPropertiesEntity;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.CourtCaseEntity;
 import uk.gov.justice.probation.courtcaseservice.jpa.repository.CourtCaseRepository;
-
-import java.nio.file.Files;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -103,6 +102,7 @@ public class CourtCaseControllerPutIntTest extends BaseIntTest {
             .body("previouslyKnownTerminationDate", equalTo(LocalDate.of(2018, 6, 24).format(DateTimeFormatter.ISO_LOCAL_DATE)))
             .body("suspendedSentenceOrder", equalTo(true))
             .body("breach", equalTo(true))
+            .body("defendantType", equalTo("PERSON"))
             .body("defendantName", equalTo(DEFENDANT_NAME))
             .body("defendantAddress.line1", equalTo(ADDRESS.getLine1()))
             .body("defendantAddress.line2", equalTo(ADDRESS.getLine2()))

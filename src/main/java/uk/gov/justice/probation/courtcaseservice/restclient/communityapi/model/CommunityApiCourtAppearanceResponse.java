6da0144c4bef738f35b3c68f892c285d60ec7226
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-10-15 09:16:37 +0100

    👌🏻 PR comments

--- src/main/java/uk/gov/justice/probation/courtcaseservice/restclient/communityapi/model/CommunityApiCourtAppearanceResponse.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/restclient/communityapi/model/CommunityApiCourtAppearanceResponse.java
@@ -2,8 +2,6 @@ package uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model;
 
 
 import java.time.LocalDateTime;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.annotations.ApiModel;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,12 +9,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import uk.gov.justice.probation.courtcaseservice.service.model.KeyValue;
 
-@ApiModel(description = "Court Appearances from Community API")
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommunityApiCourtAppearanceResponse {
 
     private final Long courtAppearanceId;

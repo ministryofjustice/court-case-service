b2c1a111424dd7a98086e4a2d3ab395d01bd71bb
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-10-16 14:44:04 +0100

    ✨PIC-863 Add support for NAME_DOB_ALIAS

--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/model/MatchType.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/model/MatchType.java
@@ -2,6 +2,8 @@ package uk.gov.justice.probation.courtcaseservice.service.model;
 
 public enum MatchType {
     NAME_DOB,
+    /** Matches to all the parameters supplied but at least one from the one of the aliases associated to the offender */
+    NAME_DOB_ALIAS,
     HMPPS_KEY,
     EXTERNAL_KEY,
     NAME,

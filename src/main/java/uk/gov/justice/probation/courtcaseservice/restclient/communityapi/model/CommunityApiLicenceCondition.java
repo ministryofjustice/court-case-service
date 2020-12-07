b477434b63519abdaef9d6bf7efddf824c22eedd
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2020-12-07 12:59:54 +0000

    ✨ PIC-914 add to LicenceCondition (#273)
    * ✨ PIC-914 add to LicenceCondition
    
    Took 14 minutes
    
    * 🔐 PIC-914 fix vulnerability in groovy from rest-assured
    
    * 🔐 PIC-914 fix vulnerability in groovy from rest-assured

--- src/main/java/uk/gov/justice/probation/courtcaseservice/restclient/communityapi/model/CommunityApiLicenceCondition.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/restclient/communityapi/model/CommunityApiLicenceCondition.java
@@ -1,11 +1,13 @@
 package uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model;
 
+import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uk.gov.justice.probation.courtcaseservice.service.model.KeyValue;
 
 @Data
 @Builder
@@ -14,22 +16,14 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CommunityApiLicenceCondition {
 
-    private MainCatTypeDetail licenceConditionTypeMainCat;
+    private String licenceConditionNotes;
+    private LocalDate startDate;
+    private String commencementNotes;
+    private String terminationNotes;
+    private KeyValue licenceConditionTypeMainCat;
+    private KeyValue licenceConditionTypeSubCat;
 
-    @ApiModelProperty(value = "Is the requirement currently active")
+    @ApiModelProperty(value = "Is the licence condition currently active")
     private Boolean active;
 
-    @Data
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class MainCatTypeDetail {
-
-        @ApiModelProperty(required = true)
-        private String description;
-
-        @ApiModelProperty(required = true)
-        private String code;
-
-    }
 }

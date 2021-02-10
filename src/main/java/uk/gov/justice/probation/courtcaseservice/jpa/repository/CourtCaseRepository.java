d36862fb10ed871027b859ee105eee8564225fbd
John Evans <john.evans@digital.justice.gov.uk>
2021-02-10 17:11:03 +0000

    🐛 PIC-1102: Fix for session date/time updates not being reflected by get cases endpoint (#326)

--- src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/repository/CourtCaseRepository.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/repository/CourtCaseRepository.java
@@ -12,36 +12,39 @@ import java.util.Optional;
 @Repository
 public interface CourtCaseRepository extends CrudRepository<CourtCaseEntity, Long> {
     @Query(value = "select cc.*, grouped_cases.min_created as first_created from court_case cc " +
-            "inner join (select max(created) as max_created,  min(created) as min_created, case_no from court_case group_cc " +
+            "inner join (select max(created) as max_created,  min(created) as min_created, case_no, court_code from court_case group_cc " +
             "where group_cc.case_no = :caseNo " +
             "and group_cc.court_code = :courtCode " +
             "group by case_no, court_code) grouped_cases " +
             "on cc.case_no = grouped_cases.case_no " +
-            "and cc.created = grouped_cases.max_created " +
+            "and cc.court_code = grouped_cases.court_code " +
+            "where cc.created = grouped_cases.max_created " +
             "and cc.deleted = false",
             nativeQuery = true)
     Optional<CourtCaseEntity> findByCourtCodeAndCaseNo(String courtCode, String caseNo);
 
     @Query(value = "select cc.*, grouped_cases.min_created as first_created from court_case cc " +
-            "inner join (select min(created) as min_created, max(created) as max_created, case_no from court_case group_cc " +
-            "where session_start_time >= :sessionStartAfter " +
-            "and session_start_time < :sessionStartBefore " +
-            "and created >= :createdAfter " +
+            "inner join (select min(created) as min_created, max(created) as max_created, case_no, court_code from court_case group_cc " +
+            "where created >= :createdAfter " +
             "and group_cc.court_code = :courtCode " +
             "group by case_no, court_code) grouped_cases " +
             "on cc.case_no = grouped_cases.case_no " +
+            "and cc.court_code = grouped_cases.court_code " +
+            "where session_start_time >= :sessionStartAfter " +
+            "and session_start_time < :sessionStartBefore " +
             "and cc.created = grouped_cases.max_created " +
             "and cc.deleted = false",
             nativeQuery = true)
     List<CourtCaseEntity> findByCourtCodeAndSessionStartTime(String courtCode, LocalDateTime sessionStartAfter, LocalDateTime sessionStartBefore, LocalDateTime createdAfter);
 
     @Query(value = "select cc.*, grouped_cases.min_created as first_created from court_case cc "
-            + "inner join (select min(created) as min_created, max(created) as max_created, case_no from court_case group_cc "
+            + "inner join (select min(created) as min_created, max(created) as max_created, case_no, court_code from court_case group_cc "
             + "where crn = :crn "
             + "and case_no != :caseNo "
-            + "and session_start_time >= current_date "
             + "group by case_no, court_code) grouped_cases "
             + "on cc.case_no = grouped_cases.case_no "
+            + "and cc.court_code = grouped_cases.court_code "
+            + "where session_start_time >= current_date "
             + "and cc.created = grouped_cases.max_created "
             + "and cc.deleted = false",
             nativeQuery= true)

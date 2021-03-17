3f72c0bceaaa08c73c04042f8351a7b692b5d387
John Evans <john.evans@digital.justice.gov.uk>
2021-03-17 17:22:22 +0000

    🔥 PIC-1172: Remove currentDisposal field from CommunityApiOffenderResponse

--- src/main/java/uk/gov/justice/probation/courtcaseservice/restclient/communityapi/model/CommunityApiOffenderResponse.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/restclient/communityapi/model/CommunityApiOffenderResponse.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,7 +25,4 @@ public class CommunityApiOffenderResponse {
     private String surname;
     private LocalDate dateOfBirth;
     private CommunityApiContactDetails contactDetails;
-    @JsonDeserialize(using = NumericBooleanDeserializer.class)
-    private boolean currentDisposal;
-
 }

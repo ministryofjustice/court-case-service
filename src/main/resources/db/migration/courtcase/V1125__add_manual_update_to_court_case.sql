b8e7053ff7092d4651b5babe9a8b64e64a0a337b
John Evans <john.evans@digital.justice.gov.uk>
2021-03-29 17:40:12 +0100

    🗃 PIC-1249: Update CourtCaseRepository case list query to use manual_update and update index

--- src/main/resources/db/migration/courtcase/V1125__add_manual_update_to_court_case.sql
+++ src/main/resources/db/migration/courtcase/V1125__add_manual_update_to_court_case.sql
@@ -4,4 +4,7 @@ ALTER TABLE COURT_CASE ADD COLUMN manual_update boolean DEFAULT false;
 UPDATE COURT_CASE SET manual_update = CREATED_BY LIKE '%(prepare-a-case-for-court)';
 ALTER TABLE COURT_CASE ALTER COLUMN manual_update SET not null;
 
+drop index court_case_list_idx;
+create index court_case_list_idx on court_case (created, court_code, manual_update);
+
 COMMIT;

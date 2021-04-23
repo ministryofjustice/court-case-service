4cd9d596f669fce9111df519a4fb6cc1d3be42f4
John Evans <john.evans@digital.justice.gov.uk>
2021-04-23 16:53:11 +0100

    📈 PIC-1270: Log deployment message with version on application start (#390)
    * 📈 PIC-1270: Log deployment message with version on application start
    
    * 📈 PIC-1270: Log deployment message with version on application start

---
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/DeploymentLoggerTest.java
@@ -0,0 +1,31 @@
+package uk.gov.justice.probation.courtcaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.info.BuildProperties;
+
+import java.net.UnknownHostException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DeploymentLoggerTest {
+
+    @Mock
+    private BuildProperties buildProperties;
+
+    @Test
+    public void shouldLogInfo() throws UnknownHostException {
+        when(buildProperties.getVersion()).thenReturn("expected_version");
+        final var logger = new DeploymentLogger(buildProperties);
+
+        TestAppender.events.clear();
+        logger.onApplicationEvent(null);
+        assertThat(TestAppender.events.size()).isEqualTo(1);
+        assertThat(TestAppender.events.get(0).toString()).startsWith("[INFO] Starting CourtCaseServiceApplication expected_version using Java ");
+    }
+
+}

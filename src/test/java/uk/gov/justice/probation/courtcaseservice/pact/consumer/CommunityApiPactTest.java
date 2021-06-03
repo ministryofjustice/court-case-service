11c3bc68e4b27c92b71e284bf2667839205c8490
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2021-06-03 22:17:12 +0100

    ✨ PIC-1426 PACT consumer tests

--- src/test/java/uk/gov/justice/probation/courtcaseservice/pact/consumer/CommunityApiPactTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/pact/consumer/CommunityApiPactTest.java
@@ -16,8 +16,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static uk.gov.justice.probation.courtcaseservice.testUtil.DateHelper.standardDateOf;
 
 @ExtendWith(PactConsumerTestExt.class)
 @PactTestFor(providerName = "community-api")
@@ -44,7 +42,7 @@ class CommunityApiPactTest {
             .given("an NSI exists for CRN X320741 and conviction id 2500295345")
             .uponReceiving("a request for a NSIs by CRN and conviction ID")
             .path("/secure/offenders/crn/X320741/convictions/2500295345/nsis")
-            .query("nsiCodes=BRE&nsiCodes=BRES")
+            .query("nsiCodes=BRE")
             .method("GET")
             .willRespondWith()
             .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
@@ -54,17 +52,36 @@ class CommunityApiPactTest {
     }
 
     @Pact(provider="community-api", consumer="court-case-service")
-    public RequestResponsePact getProbationStatusDetail(PactDslWithProvider builder) {
+    public RequestResponsePact getProbationStatusDetailCurrent(PactDslWithProvider builder) {
 
         var body = new PactDslJsonBody()
             .booleanType("preSentenceActivity", "inBreach", "awaitingPsr")
-            .stringType("status")
-            .date("previouslyKnownTerminationDate");
+            .stringType("status");
 
         return builder
             .given("probation status detail is available for CRN X320741")
-            .uponReceiving("a request for a probation status detail by CRN")
+            .uponReceiving("a request for a CURRENT probation status detail by CRN")
             .path("/secure/offenders/crn/X320741/probationStatus")
+            .method("GET")
+            .willRespondWith()
+            .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+            .body(body)
+            .status(200)
+            .toPact();
+    }
+
+    @Pact(provider="community-api", consumer="court-case-service")
+    public RequestResponsePact getProbationStatusDetailPreviouslyKnown(PactDslWithProvider builder) {
+
+        var body = new PactDslJsonBody()
+            .booleanType("preSentenceActivity", "awaitingPsr")
+            .date("previouslyKnownTerminationDate","yyyy-MM-dd")
+            .stringType("status");
+
+        return builder
+            .given("probation status detail is available for CRN CRN40")
+            .uponReceiving("a request for a PREVIOUSLY KNOWN probation status detail by CRN")
+            .path("/secure/offenders/crn/CRN40/probationStatus")
             .method("GET")
             .willRespondWith()
             .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
@@ -77,18 +94,29 @@ class CommunityApiPactTest {
     @Test
     void getNsis(MockServer mockServer) throws IOException {
         var httpResponse = Request
-            .Get(mockServer.getUrl() + "/secure/offenders/crn/X320741/convictions/2500295345/nsis?nsiCodes=BRE&nsiCodes=BRES")
+            .Get(mockServer.getUrl() + "/secure/offenders/crn/X320741/convictions/2500295345/nsis?nsiCodes=BRE")
             .execute()
             .returnResponse();
 
         assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     }
 
-    @PactTestFor(pactMethod = "getProbationStatusDetail")
+    @PactTestFor(pactMethod = "getProbationStatusDetailCurrent")
     @Test
-    void getProbationStatusDetail(MockServer mockServer) throws IOException {
+    void getProbationStatusDetailCurrent(MockServer mockServer) throws IOException {
         var httpResponse = Request
             .Get(mockServer.getUrl() + "/secure/offenders/crn/X320741/probationStatus")
+            .execute()
+            .returnResponse();
+
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+    }
+
+    @PactTestFor(pactMethod = "getProbationStatusDetailPreviouslyKnown")
+    @Test
+    void getProbationStatusDetailPreviouslyKnown(MockServer mockServer) throws IOException {
+        var httpResponse = Request
+            .Get(mockServer.getUrl() + "/secure/offenders/crn/CRN40/probationStatus")
             .execute()
             .returnResponse();
 

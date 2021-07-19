cb12d3c44e79393a0a29eadbe99b1ee6bfa2aa04
John Evans <john.evans@digital.justice.gov.uk>
2021-07-19 19:12:01 +0100

    ✅ PIC-1135: Complete CustodyService implementation

--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/CustodyService.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/CustodyService.java
@@ -3,12 +3,15 @@ package uk.gov.justice.probation.courtcaseservice.service;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 import uk.gov.justice.probation.courtcaseservice.restclient.ConvictionRestClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.CustodyRestClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.OffenderRestClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.OffenderRestClientFactory;
 import uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model.CommunityApiOffenderResponse;
 import uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model.OtherIds;
+import uk.gov.justice.probation.courtcaseservice.service.exceptions.ExpectedCustodyNotFoundException;
+import uk.gov.justice.probation.courtcaseservice.service.exceptions.NomsNumberNotAvailableException;
 import uk.gov.justice.probation.courtcaseservice.service.model.Conviction;
 import uk.gov.justice.probation.courtcaseservice.service.model.Custody;
 
@@ -26,16 +29,19 @@ public class CustodyService {
     }
 
     public Mono<Custody> getCustody(String crn, Long convictionId) {
-        if (convictionRestClient.getConviction(crn, convictionId)
-                .map(Conviction::getCustodialType)
-                .block() == null)
-            return Mono.empty();
 
+        final var inCustodyMono = convictionRestClient.getConviction(crn, convictionId)
+                .mapNotNull(Conviction::getCustodialType);
 
-        final var nomsNumber = offenderRestClient.getOffender(crn)
+        final var nomsMono = offenderRestClient.getOffender(crn)
                 .map(CommunityApiOffenderResponse::getOtherIds)
-                .map(OtherIds::getNomsNumber)
-                .block();
-        return custodyRestClient.getCustody(nomsNumber);
+                .mapNotNull(OtherIds::getNomsNumber)
+                .switchIfEmpty(Mono.error(new NomsNumberNotAvailableException(String.format("Could not get custody data as no NOMS number was returned from the community-api for crn '%s'", crn))));
+
+        return Mono.zip(inCustodyMono, nomsMono)
+                // We only care that T1 is not empty so can discard the value
+                .map(Tuple2::getT2)
+                .flatMap(nomsNumber -> custodyRestClient.getCustody(nomsNumber)
+                        .switchIfEmpty(Mono.error(new ExpectedCustodyNotFoundException(String.format("Expected custody data for nomsNumber '%s' was not found at prison-api", nomsNumber)))));
     }
 }
--- src/test/java/uk/gov/justice/probation/courtcaseservice/service/CustodyServiceTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/service/CustodyServiceTest.java
@@ -11,11 +11,16 @@ import uk.gov.justice.probation.courtcaseservice.restclient.CustodyRestClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.OffenderRestClient;
 import uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model.CommunityApiOffenderResponse;
 import uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model.OtherIds;
+import uk.gov.justice.probation.courtcaseservice.service.exceptions.ExpectedCustodyNotFoundException;
+import uk.gov.justice.probation.courtcaseservice.service.exceptions.NomsNumberNotAvailableException;
 import uk.gov.justice.probation.courtcaseservice.service.model.Conviction;
 import uk.gov.justice.probation.courtcaseservice.service.model.Custody;
 import uk.gov.justice.probation.courtcaseservice.service.model.KeyValue;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,6 +28,7 @@ class CustodyServiceTest {
 
     private static final String CRN = "CRN";
     private static final String NOMS_NUMBER = "nomnomnom";
+    private static final long CONVICTION_ID = 12345L;
     @Mock
     private ConvictionRestClient convictionRestClient;
     @Mock
@@ -32,33 +38,80 @@ class CustodyServiceTest {
 
     private CustodyService custodyService;
 
+    private static final Conviction CONVICTION_WITH_CUSTODY = Conviction.builder()
+            .custodialType(KeyValue.builder()
+                    .code("ANY")
+                    .description("Owt will do")
+                    .build())
+            .build();
+
+    private static final CommunityApiOffenderResponse OFFENDER_WITH_NOMS = CommunityApiOffenderResponse.builder()
+            .otherIds(OtherIds.builder()
+                    .nomsNumber(NOMS_NUMBER)
+                    .build())
+            .build();
+
     @BeforeEach
     public void setUp() {
         custodyService = new CustodyService(convictionRestClient, offenderRestClient, custodyRestClient);
     }
 
     @Test
-    public void whenGetCustody_thenReturnIt() {
-        final var conviction = Conviction.builder()
-                .custodialType(KeyValue.builder()
-                        .code("ANY")
-                        .description("Owt will do")
-                        .build())
-                .build();
+    public void whenGetCustody_andCustodyDatesExist_thenReturnThem() {
 
-        final var offender = CommunityApiOffenderResponse.builder()
-                .otherIds(OtherIds.builder()
-                        .nomsNumber(NOMS_NUMBER)
-                        .build())
-                .build();
         final var expectedCustody = Custody.builder().build();
 
-        when(convictionRestClient.getConviction(CRN, 12345L)).thenReturn(Mono.just(conviction));
-        when(offenderRestClient.getOffender(CRN)).thenReturn(Mono.just(offender));
+        when(convictionRestClient.getConviction(CRN, CONVICTION_ID)).thenReturn(Mono.just(CONVICTION_WITH_CUSTODY));
+        when(offenderRestClient.getOffender(CRN)).thenReturn(Mono.just(OFFENDER_WITH_NOMS));
         when(custodyRestClient.getCustody(NOMS_NUMBER)).thenReturn(Mono.just(expectedCustody));
-        final var actualCustody = custodyService.getCustody(CRN, 12345L).block();
+        final var actualCustody = custodyService.getCustody(CRN, CONVICTION_ID).block();
 
         assertThat(actualCustody).isEqualTo(expectedCustody);
+
+    }
+
+    @Test
+    public void whenGetCustody_andOffenderNotInCustody_thenReturnEmpty() {
+        final var conviction = Conviction.builder()
+                .custodialType(null)
+                .build();
+
+        when(offenderRestClient.getOffender(CRN)).thenReturn(Mono.just(OFFENDER_WITH_NOMS));
+
+        when(convictionRestClient.getConviction(CRN, CONVICTION_ID)).thenReturn(Mono.just(conviction));
+        final var actualMono = custodyService.getCustody(CRN, CONVICTION_ID);
+
+        assertThat(actualMono.blockOptional()).isEqualTo(Optional.empty());
+
+    }
+
+    @Test
+    public void whenGetCustody_andCustodyDatesExist_andOffenderHasNoNomsNumber_thenThrowException() {
+        final var offender = CommunityApiOffenderResponse.builder()
+                .otherIds(OtherIds.builder()
+                        .nomsNumber(null)
+                        .build())
+                .build();
+
+        when(convictionRestClient.getConviction(CRN, CONVICTION_ID)).thenReturn(Mono.just(CONVICTION_WITH_CUSTODY));
+        when(offenderRestClient.getOffender(CRN)).thenReturn(Mono.just(offender));
+        assertThatExceptionOfType(NomsNumberNotAvailableException.class)
+                .isThrownBy(
+                        () -> custodyService.getCustody(CRN, CONVICTION_ID).block()
+                ).withMessage("Could not get custody data as no NOMS number was returned from the community-api for crn 'CRN'");
+
+    }
+
+    @Test
+    public void whenGetCustody_andOffenderHasNomsNumber_andNoCustodyDataReturned_thenThrowException() {
+
+        when(convictionRestClient.getConviction(CRN, CONVICTION_ID)).thenReturn(Mono.just(CONVICTION_WITH_CUSTODY));
+        when(offenderRestClient.getOffender(CRN)).thenReturn(Mono.just(OFFENDER_WITH_NOMS));
+        when(custodyRestClient.getCustody(NOMS_NUMBER)).thenReturn(Mono.empty());
+        assertThatExceptionOfType(ExpectedCustodyNotFoundException.class)
+                .isThrownBy(
+                        () -> custodyService.getCustody(CRN, CONVICTION_ID).block()
+                ).withMessage("Expected custody data for nomsNumber 'nomnomnom' was not found at prison-api");
 
     }
 }

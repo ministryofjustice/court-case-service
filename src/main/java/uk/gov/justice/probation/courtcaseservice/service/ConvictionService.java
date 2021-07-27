6db937c6c0087b1d2f84f8db36fb12922ebd61b8
katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>
2021-07-27 12:15:43 +0100

    :fire: PIC-1533: remove commented code
    Signed-off-by: katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>

--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/ConvictionService.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/ConvictionService.java
@@ -62,19 +62,6 @@ public class ConvictionService {
         return combineOffenderAndConvictions(tuple4.getT1(), tuple4.getT2(), tuple4.getT3(), tuple4.getT4());
     }
 
-//    public SentenceResponse getSentenceDetail(final String crn, final Long convictionId) {
-//        var sentenceMono = Mono.zip(
-//                convictionRestClient.getAttendances(crn, convictionId),
-//                convictionRestClient.getConviction(crn, convictionId),
-//                convictionRestClient.getCurrentOrderHeader(crn, convictionId),
-//                offenderRestClient.getOffender(crn)
-//        );
-//
-//        var tuple4 = sentenceMono.blockOptional().orElseThrow(() -> new OffenderNotFoundException(crn));
-//
-//        return combineOffenderAndConvictions(tuple4.getT1(), tuple4.getT2(), tuple4.getT3(), tuple4.getT4());
-//    }
-
     public CurrentOrderHeaderResponse getCurrentOrderHeader(final String crn, final Long convictionId) {
         return convictionRestClient.getCurrentOrderHeader(crn, convictionId)
                 .blockOptional()

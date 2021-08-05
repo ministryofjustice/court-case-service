c046d72e84ed12e302a5ffbeacba47239c33d1ba
John Evans <john.evans@digital.justice.gov.uk>
2021-08-05 16:13:43 +0100

    📝 PIC-1586: Create stub endpoint for extended case (#488)
    * 📝 PIC-1586: Create stub endpoint for extended case
    
    * 📝 PIC-1586: Move offences under Defendant

---
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/controller/model/Defendant.java
@@ -0,0 +1,34 @@
+package uk.gov.justice.probation.courtcaseservice.controller.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.justice.probation.courtcaseservice.jpa.entity.DefendantType;
+import uk.gov.justice.probation.courtcaseservice.jpa.entity.NamePropertiesEntity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor
+public class Defendant {
+    private final NamePropertiesEntity name;
+    private final LocalDate dateOfBirth;
+    private final AddressRequest address;
+    private final String probationStatus;
+    private final DefendantType type;
+    private final String sex;
+    private final String crn;
+    private final String pnc;
+    private final String cro;
+    private final LocalDate previouslyKnownTerminationDate;
+    private final Boolean suspendedSentenceOrder;
+    private final Boolean breach;
+    private final Boolean preSentenceActivity;
+    private final Boolean awaitingPsr;
+    private final List<OffenceRequest> offences;
+}
---
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/controller/model/HearingDay.java
@@ -0,0 +1,18 @@
+package uk.gov.justice.probation.courtcaseservice.controller.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor
+public class HearingDay {
+    private final LocalDateTime sessionStartTime;
+    private final String listNo;
+}

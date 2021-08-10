dbd00187c4c83c275d9aded18757598016140758
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2021-08-10 12:05:00 +0100

    Pic 1582 populate new entities (#493)
    * ✨ PIC-1581 add new JPA entities
    
    * ✨ PIC-1581 fix field name
    
    * ✨ PIC-1582 populates the new DefendantEntity, HearingEntity collections on the CourtCase
    
    * ✨ PIC-1582 added test
    
    * ✨ PIC-1582 fixed autoincrement values

--- src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/NamePropertiesEntity.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/NamePropertiesEntity.java
@@ -1,6 +1,11 @@
 package uk.gov.justice.probation.courtcaseservice.jpa.entity;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -20,4 +25,12 @@ public class NamePropertiesEntity implements Serializable {
     private String forename2;
     private String forename3;
     private String surname;
+
+    @JsonIgnore
+    public String getFullName() {
+        return Stream.of(title, forename1, forename2, forename3, surname)
+            .filter(Objects::nonNull)
+            .collect(Collectors.joining(" "));
+    }
+
 }

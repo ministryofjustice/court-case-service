8916f4ad2c2d3ccea3b636f8f94bccbb557a4e8b
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2021-08-27 13:46:49 +0100

    ✨ PIC-1640 fixes db deployment (#508)

--- src/main/resources/db/migration/courtcase/V2005__add_defendant_uuid.sql
+++ src/main/resources/db/migration/courtcase/V2005__add_defendant_uuid.sql
@@ -1,7 +1,9 @@
 BEGIN;
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-    alter table DEFENDANT add column DEFENDANT_ID UUID not null;
+    alter table DEFENDANT add column DEFENDANT_ID UUID;
 
     -- uuid_generate_v4 on 3 random numbers, uuid_generate_v1 is based on MAC address of computer, timestamp and a random number,
     update DEFENDANT SET DEFENDANT_ID = uuid_generate_v4() where DEFENDANT_ID is null;
+
+    ALTER TABLE DEFENDANT ALTER COLUMN DEFENDANT_ID SET NOT NULL;
 COMMIT;

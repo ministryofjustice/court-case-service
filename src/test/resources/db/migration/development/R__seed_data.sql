4f5134de95bc80c2b8edcb205748f3f181c66596
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2021-10-29 14:19:09 +0100

    ✨ PIC-1692 - remove case_no and court code R_Seed_data (#574)
    * ✨ PIC-1692 - remove case_no and court code R_Seed_data
    
    * ✨ PIC-1692 - added a defendant and use negative numbers for pre-loaded cases
    
    * ✨ PIC-1692 - use UUIDs
    
    * ✨ PIC-1692 - use UUIDs

--- src/test/resources/db/migration/development/R__seed_data.sql
+++ src/test/resources/db/migration/development/R__seed_data.sql
@@ -9,60 +9,37 @@ INSERT INTO court (id, name, court_code) VALUES (1142409, 'Sheffield Magistrates
 INSERT INTO court_case
     (id, case_id, case_no, court_code, court_room, session_start_time, probation_status, previously_known_termination_date, suspended_sentence_order, breach, defendant_name, defendant_address, crn, pnc, cro, list_no, defendant_dob, defendant_sex, nationality_1, nationality_2, source_type)
 VALUES
-    (1, 1168460, 1600028912, 'SHF', 1, '2019-12-14 09:00', 'No record', null, false, false, 'Mr Julian Cone', '{"line1": "77", "line2":"Castle Crescent", "postcode":"DN1 2SD","line3":"Doncaster", "line4": "South Yorkshire"}', 'X320741', 'A/1234560BA', '18763/79J', '3rd', '1958-10-31', 'M', 'British', null, 'LIBRA');
+    (-1, 1168460, 1600028912, 'SHF', 1, '2019-12-14 09:00', 'No record', null, false, false, 'Mr Julian Cone', '{"line1": "77", "line2":"Castle Crescent", "postcode":"DN1 2SD","line3":"Doncaster", "line4": "South Yorkshire"}', 'X320741', 'A/1234560BA', '18763/79J', '3rd', '1958-10-31', 'M', 'British', null, 'LIBRA');
 
 INSERT INTO court_case
     (id, case_id, case_no, court_code, court_room, session_start_time, probation_status, previously_known_termination_date, suspended_sentence_order, breach, defendant_name, defendant_address, crn, pnc, cro, list_no, defendant_dob, defendant_sex, nationality_1, nationality_2, source_type)
 VALUES
-    (2, 1246257, 1600028974, 'SHF', 1, '2020-01-13 09:00', 'Current', null, true, true, 'Mr Dylan Adam Armstrong', '{"line1": "27", "line2":"Elm Place", "postcode":"ad21 5dr","line3":"Bangor"}', 'X320741', 'A/1234560BA', '311462/13E', '1st', '1977-12-11', 'M', 'British', null, 'LIBRA');
+    (-2, 1246257, 1600028974, 'SHF', 1, '2020-01-13 09:00', 'Current', null, true, true, 'Mr Dylan Adam Armstrong', '{"line1": "27", "line2":"Elm Place", "postcode":"ad21 5dr","line3":"Bangor"}', 'X320741', 'A/1234560BA', '311462/13E', '1st', '1977-12-11', 'M', 'British', null, 'LIBRA');
 
 INSERT INTO court_case
 (id, case_id, case_no, court_code, court_room, session_start_time, probation_status, previously_known_termination_date, suspended_sentence_order, breach, defendant_name, defendant_address, crn, pnc, cro, list_no, defendant_dob, defendant_sex, nationality_1, nationality_2, source_type)
 VALUES
-    (3, 1246258, 1600028956, 'SHF', 1, '2020-01-13 09:00', 'Previously known', '2019-12-14 09:00', true, true, 'Mr Joe Bloggs', '{"line1": "103", "line2":"Warwick Avenue", "postcode":"S1 6UA","line3":"Sheffield"}', 'X320741', 'D/1234560BA', '311499/16F', '1st', '1983-02-02', 'M', 'Polish', null, 'LIBRA');
+    (-3, 1246258, 1600028956, 'SHF', 1, '2020-01-13 09:00', 'Previously known', '2019-12-14 09:00', true, true, 'Mr Joe Bloggs', '{"line1": "103", "line2":"Warwick Avenue", "postcode":"S1 6UA","line3":"Sheffield"}', 'X320741', 'D/1234560BA', '311499/16F', '1st', '1983-02-02', 'M', 'Polish', null, 'LIBRA');
 
 INSERT INTO court_case
     (id, case_id, case_no, court_code, court_room, session_start_time, probation_status, previously_known_termination_date, suspended_sentence_order, breach, defendant_name, defendant_address, crn, pnc, cro, list_no, defendant_dob, defendant_sex, nationality_1, nationality_2, source_type)
 VALUES
-    (4, 1246273, 1600028920, 'SHF', 1, '2020-01-13 09:00', 'No record', null, false, false, 'Mr Ollie Test', '{"line1": "65", "line2":"Relish Avenue", "postcode":"LS3 5FA","line3":"Leeds", "line4": "West Yorkshire"}', 'X320741', 'B/1234560BB', '317462/19X', '2nd', '1996-12-03', 'M', 'British', null, 'LIBRA');
+    (-4, 1246273, 1600028920, 'SHF', 1, '2020-01-13 09:00', 'No record', null, false, false, 'Mr Ollie Test', '{"line1": "65", "line2":"Relish Avenue", "postcode":"LS3 5FA","line3":"Leeds", "line4": "West Yorkshire"}', 'X320741', 'B/1234560BB', '317462/19X', '2nd', '1996-12-03', 'M', 'British', null, 'LIBRA');
 
 INSERT INTO court_case
     (id, case_id, case_no, court_code, court_room, session_start_time, probation_status, previously_known_termination_date, suspended_sentence_order, breach, defendant_name, defendant_address, crn, pnc, cro, list_no, defendant_dob, defendant_sex, nationality_1, nationality_2, source_type)
 VALUES
-    (5, 1248278, 1600029021, 'SHF', 1, '2020-01-13 09:00', 'No record', null, false, false, 'Mr Ureet JMBALERNAEU', '{"line1": "56", "line2":"Henderson Walk", "postcode":"S2 6GA","line3":"Sheffield"}', 'X320741', 'D/2134650CA', '766412/20R', '3rd', '1966-03-03', 'M', 'British', null, 'LIBRA');
+    (-5, '8f1f74be-7c63-4226-abbe-a4292d3d2592', 1600029021, 'SHF', 1, '2020-01-13 09:00', 'No record', null, false, false, 'Mr Ureet JMBALERNAEU', '{"line1": "56", "line2":"Henderson Walk", "postcode":"S2 6GA","line3":"Sheffield"}', 'X320741', 'D/2134650CA', '766412/20R', '3rd', '1966-03-03', 'M', 'British', null, 'COMMON_PLATFORM');
 
+INSERT INTO DEFENDANT (id, court_case_id, DEFENDANT_ID, defendant_name, name, crn, pnc, cro, type, sex, probation_status, date_of_birth)
+VALUES (-2, -5, 'e10762d4-b874-43e1-9b51-0997b888d134', 'Mr Ureet JMBALERNAEU', '{"title": "Mr", "surname": "JMBALERNAEU", "forename1": "Ureet"}', 'X320741', 'D/2134650CA', '999999/20R', 'PERSON', 'M', 'NO_RECORD', '1966-03-03');
 
-INSERT INTO OFFENCE (
-    COURT_CASE_ID,
-	OFFENCE_TITLE,
-    OFFENCE_SUMMARY,
-    ACT,
-	SEQUENCE_NUMBER
-	) VALUES (
-	    1,
-        'Theft from a shop',
-        'On 01/01/2015 at own, stole article, to the value of £987.00, belonging to person.',
-        'Contrary to section 1(1) and 7 of the Theft Act 1968.',
-        1
-	);
-
-INSERT INTO OFFENCE (
-    COURT_CASE_ID,
-	OFFENCE_TITLE,
-    OFFENCE_SUMMARY,
-    ACT,
-	SEQUENCE_NUMBER
-	) VALUES (
-        1,
-        'Theft from a different shop',
-        'On 02/01/2015 at own, stole article, to the value of £987.00, belonging to person.',
-        'Contrary to section 1(1) and 7 of the Theft Act 1968.',
-        2
-	);
+INSERT INTO DEFENDANT (id, court_case_id, DEFENDANT_ID, defendant_name, name, pnc, cro, type, sex, probation_status, date_of_birth)
+VALUES (-1, -5, '183b84fb-7142-4f41-8a9b-6f3c9905df97', 'Mr David BOWIE', '{"title": "Mr", "surname": "BOWIE", "forename1": "David"}', 'E/2134650CA', '888888/20R', 'PERSON', 'M', 'NO_RECORD', '1969-03-03');
 
 INSERT INTO offender_match_group
-( id, case_no, court_code, created, last_updated, created_by, last_updated_by, deleted, "version")
-VALUES( 1, '1600029021', 'SHF', now(), now(), 'R_seed_data', '', false, 0);
+( id, case_id, defendant_id, created, last_updated, created_by, last_updated_by, deleted, "version")
+VALUES( 1, '8f1f74be-7c63-4226-abbe-a4292d3d2592', 'e10762d4-b874-43e1-9b51-0997b888d134' , now(), now(), 'R_seed_data', '', false, 0);
 
 INSERT INTO offender_match
 (confirmed, crn, cro, match_type, pnc, group_id, created, last_updated, created_by, last_updated_by, deleted, "version", rejected)

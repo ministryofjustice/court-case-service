4a17cd358b3f2afaacdbdf1cc2219834542b0920
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2021-11-12 15:20:23 +0000

    ✨ PIC-1859 defendant sex may be null when received

---
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/controller/model/DefendantTest.java
@@ -0,0 +1,42 @@
+package uk.gov.justice.probation.courtcaseservice.controller.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class DefendantTest {
+
+    @Test
+    void givenNullSex_whenGet_thenReturnNotKnown() {
+        // Consumers require "M", "F", "N", "NS" etc, via PACTs
+        assertThat(Defendant.builder().build().getSex()).isEqualTo("N");
+        assertThat(Defendant.builder().sex("   ").build().getSex()).isEqualTo("N");
+    }
+
+    @Test
+    void givenFemaleSex_whenGet_thenReturnM() {
+        assertThat(Defendant.builder().sex("female").build().getSex()).isEqualTo("F");
+        assertThat(Defendant.builder().sex("FEMALE").build().getSex()).isEqualTo("F");
+        assertThat(Defendant.builder().sex("F").build().getSex()).isEqualTo("F");
+    }
+
+    @Test
+    void givenMaleSex_whenGet_thenReturnF() {
+        assertThat(Defendant.builder().sex("maLE").build().getSex()).isEqualTo("M");
+        assertThat(Defendant.builder().sex("MALE").build().getSex()).isEqualTo("M");
+        assertThat(Defendant.builder().sex("M").build().getSex()).isEqualTo("M");
+    }
+
+    @Test
+    void givenNotKnownSex_whenGet_thenReturnN() {
+        assertThat(Defendant.builder().sex("NOT_KNOWN").build().getSex()).isEqualTo("N");
+        assertThat(Defendant.builder().sex("N").build().getSex()).isEqualTo("N");
+    }
+
+    @Test
+    void givenNotSpecifiedSex_whenGet_thenReturnNS() {
+        assertThat(Defendant.builder().sex("NOT_SPECIFIED").build().getSex()).isEqualTo("NS");
+        assertThat(Defendant.builder().sex("NS").build().getSex()).isEqualTo("NS");
+    }
+
+}
--- src/test/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/SexTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/SexTest.java
@@ -19,6 +19,18 @@ class SexTest {
     }
 
     @Test
+    void whenNotSpecified_thenReturn() {
+        assertThat(Sex.fromString("NS")).isSameAs(Sex.NOT_SPECIFIED);
+        assertThat(Sex.fromString("NOT_SPECIFIED")).isSameAs(Sex.NOT_SPECIFIED);
+    }
+
+    @Test
+    void whenNotKnown_thenReturn() {
+        assertThat(Sex.fromString("N")).isSameAs(Sex.NOT_KNOWN);
+        assertThat(Sex.fromString("NOT_KNOWN")).isSameAs(Sex.NOT_KNOWN);
+    }
+
+    @Test
     void whenUnexpected_thenReturnNotKnown() {
         assertThat(Sex.fromString(null)).isSameAs(Sex.NOT_KNOWN);
         assertThat(Sex.fromString("     ")).isSameAs(Sex.NOT_KNOWN);

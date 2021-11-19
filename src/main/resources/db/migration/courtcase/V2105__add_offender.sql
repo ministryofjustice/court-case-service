a7551913d3c84a9e422044d489a1e797b35cc51b
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2021-11-19 11:48:49 +0000

    🔧 Alter migration SQL

--- src/main/resources/db/migration/courtcase/V2105__add_offender.sql
+++ src/main/resources/db/migration/courtcase/V2105__add_offender.sql
@@ -18,15 +18,17 @@ BEGIN;
     );
 
     insert into OFFENDER ( CRN, PREVIOUSLY_KNOWN_TERMINATION_DATE, PROBATION_STATUS, SUSPENDED_SENTENCE_ORDER, BREACH, PRE_SENTENCE_ACTIVITY, AWAITING_PSR, CREATED_BY)
-        SELECT d.crn, d.PREVIOUSLY_KNOWN_TERMINATION_DATE, d.PROBATION_STATUS, d.SUSPENDED_SENTENCE_ORDER, d.BREACH, d.PRE_SENTENCE_ACTIVITY, d.AWAITING_PSR
+        SELECT crn, PREVIOUSLY_KNOWN_TERMINATION_DATE, PROBATION_STATUS, coalesce(SUSPENDED_SENTENCE_ORDER, false) as SUSPENDED_SENTENCE_ORDER, coalesce(BREACH, false) as BREACH,
+                coalesce(PRE_SENTENCE_ACTIVITY, FALSE) as PRE_SENTENCE_ACTIVITY, AWAITING_PSR, '(migration)'
         FROM defendant d
             INNER JOIN
-                ( SELECT d2.crn, MAX(d2.created) AS latest FROM defendant d2 where d2.crn is not null GROUP BY d2.crn )
-                AS grouped_defendant
-            ON  grouped_defendant.crn = d.crn
-            AND grouped_defendant.latest = d.created ;
+                ( SELECT d2.crn inner_crn, MAX(d2.created) AS latest FROM defendant d2 where d2.crn is not null GROUP BY d2.crn )
+            AS grouped_defendant
+            ON grouped_defendant.inner_crn = d.crn
+            AND grouped_defendant.latest = d.created;
 
     ALTER TABLE DEFENDANT ADD CONSTRAINT fk_defendant_offender
-                    FOREIGN KEY (CRN)
-                    REFERENCES OFFENDER (CRN);
+        FOREIGN KEY (CRN)
+        REFERENCES OFFENDER (CRN);
+
 COMMIT;

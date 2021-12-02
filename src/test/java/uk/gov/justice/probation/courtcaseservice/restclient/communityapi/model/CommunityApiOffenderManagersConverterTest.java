cc922c33dc4a15190038ecfd3f780be640d5831c
John Evans <john.evans@digital.justice.gov.uk>
2021-12-02 12:54:08 +0000

    🐛 PIC-1936: Ignore unknown properties when deserialising with ObjectMapper

---
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/restclient/communityapi/model/CommunityApiOffenderManagersConverterTest.java
@@ -0,0 +1,27 @@
+package uk.gov.justice.probation.courtcaseservice.restclient.communityapi.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CommunityApiOffenderManagersConverterTest {
+
+    @Test
+    public void shouldIgnoreUnknownValues() {
+        final var communityApiOffenderManagersConverter = new CommunityApiOffenderManagersConverter();
+        final List<Map<String, Object>> maps = Arrays.asList(
+                Map.of(
+                        "staffCode", "staff code",
+                        "unknownValue", "unknown value"
+                )
+        );
+        final var actual = communityApiOffenderManagersConverter.convert(maps);
+
+        assertThat(actual.getOffenderManagers().get(0).getStaffCode()).isEqualTo("staff code");
+    }
+
+}

211a9ae2fd7ffb7edae74fb42dd05ae5b143964c
John Evans <john.evans@digital.justice.gov.uk>
2021-12-02 09:43:52 +0000

    🔀 🗃 PIC-1651: Enable retry (#615)

--- src/main/kotlin/uk/gov/justice/probation/courtcaseservice/CourtCaseServiceApplication.kt
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/CourtCaseServiceApplication.kt
@@ -3,9 +3,11 @@ package uk.gov.justice.probation.courtcaseservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.retry.annotation.EnableRetry
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRetry
 open class CourtCaseServiceApplication
 
 fun main(args: Array<String>) {

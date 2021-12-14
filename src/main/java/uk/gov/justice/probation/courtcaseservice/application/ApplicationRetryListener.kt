fb7ad0aa107c5d111c6a0bf32db974e2342b232a
John Evans <john.evans@digital.justice.gov.uk>
2021-12-14 12:23:07 +0000

    🔀 🐛 PIC-1946: Fix retry listener configuration (#626)

--- src/main/java/uk/gov/justice/probation/courtcaseservice/application/ApplicationRetryListener.kt
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/application/ApplicationRetryListener.kt
@@ -1,26 +1,15 @@
 package uk.gov.justice.probation.courtcaseservice.application
 
 import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.retry.RetryCallback
 import org.springframework.retry.RetryContext
 import org.springframework.retry.RetryListener
-import org.springframework.retry.support.RetryTemplate
+import org.springframework.stereotype.Component
 
-@Configuration
-open class RetryConfig {
-    @Bean
-    open fun retryTemplate(): RetryTemplate {
-        val retryTemplate = RetryTemplate();
-        retryTemplate.registerListener(AppRetryListener());
-        return retryTemplate;
-    }
-}
-
-class AppRetryListener : RetryListener {
+@Component
+class ApplicationRetryListener : RetryListener {
     companion object {
-        private val log = LoggerFactory.getLogger(AppRetryListener::class.java)
+        private val log = LoggerFactory.getLogger(ApplicationRetryListener::class.java)
     }
 
     override fun <T : Any?, E : Throwable?> open(context: RetryContext?, callback: RetryCallback<T, E>?): Boolean {

1eebbe43aca3f27655c6441c2e248f7afce0876a
John Evans <john.evans@digital.justice.gov.uk>
2022-02-23 16:36:54 +0000

    🚚 PIC-2023: Undo redundant delombok

--- src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/BaseImmutableEntity.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/BaseImmutableEntity.java
@@ -3,6 +3,7 @@ package uk.gov.justice.probation.courtcaseservice.jpa.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Immutable;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Data
+@SuperBuilder
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Immutable
@@ -29,48 +31,4 @@ public class BaseImmutableEntity {
     @CreatedBy
     private String createdBy;
 
-    protected BaseImmutableEntity(BaseImmutableEntityBuilder<?, ?> b) {
-        this.created = b.created;
-        this.createdBy = b.createdBy;
-    }
-
-    public static BaseImmutableEntityBuilder<?, ?> builder() {
-        return new BaseImmutableEntityBuilderImpl();
-    }
-
-    public static abstract class BaseImmutableEntityBuilder<C extends BaseImmutableEntity, B extends BaseImmutableEntityBuilder<C, B>> {
-        private LocalDateTime created;
-        private String createdBy;
-
-        public B created(LocalDateTime created) {
-            this.created = created;
-            return self();
-        }
-
-        public B createdBy(String createdBy) {
-            this.createdBy = createdBy;
-            return self();
-        }
-
-        protected abstract B self();
-
-        public abstract C build();
-
-        public String toString() {
-            return "BaseImmutableEntity.BaseImmutableEntityBuilder(created=" + this.created + ", createdBy=" + this.createdBy + ")";
-        }
-    }
-
-    private static final class BaseImmutableEntityBuilderImpl extends BaseImmutableEntityBuilder<BaseImmutableEntity, BaseImmutableEntityBuilderImpl> {
-        private BaseImmutableEntityBuilderImpl() {
-        }
-
-        protected BaseImmutableEntityBuilderImpl self() {
-            return this;
-        }
-
-        public BaseImmutableEntity build() {
-            return new BaseImmutableEntity(this);
-        }
-    }
 }

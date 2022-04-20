111493b23c29a40cd644a064c4bd8a2ecbfcd58a
rschallapalli <98027245+rschallapalli@users.noreply.github.com>
2022-04-20 11:49:40 +0100

    PIC-2095 new GET by hearingId endpoint (#746)

--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/CourtCaseService.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/CourtCaseService.java
@@ -1,9 +1,5 @@
 package uk.gov.justice.probation.courtcaseservice.service;
 
-import org.springframework.dao.CannotAcquireLockException;
-import org.springframework.retry.annotation.Retryable;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.HearingEntity;
 import uk.gov.justice.probation.courtcaseservice.service.exceptions.EntityNotFoundException;
@@ -20,14 +16,15 @@ public interface CourtCaseService {
     HearingEntity getHearingByCaseId(String caseId) throws EntityNotFoundException;
 
     HearingEntity getHearingByCaseIdAndDefendantId(String caseId, String defendantId) throws EntityNotFoundException;
+    HearingEntity getHearingByHearingId(String hearingId) throws EntityNotFoundException;
+
+    HearingEntity getHearingByHearingIdAndDefendantId(String caseId, String defendantId) throws EntityNotFoundException;
 
     Mono<HearingEntity> createHearing(String caseId, HearingEntity updatedCase)
         throws EntityNotFoundException, InputMismatchException;
 
     Mono<HearingEntity> createUpdateHearingForSingleDefendantId(String caseId, String defendantId, HearingEntity updatedCase)
         throws EntityNotFoundException, InputMismatchException;
-
-    HearingEntity getHearingByHearingIdAndDefendantId(String hearingId, String defendantId) throws EntityNotFoundException;
 
     List<HearingEntity> filterHearings(String courtCode, LocalDate hearingDay, LocalDateTime createdAfter, LocalDateTime createdBefore);
 

039096478c7ad396d063701df1d32b80c7044851
vb-moj <104899072+vb-moj@users.noreply.github.com>
2022-08-17 10:35:42 +0100

    Pic 2292 (#883)
    * PIC-2292
    . Renamed classes and method name to use hearing instead of court case.
    
    * PIC-2292
    . Renamed classes and method name to use hearing instead of court case.
    
    * PIC-2292
    . Added judicial result table and entity.
    . Updated offence entity with judicial results.
    . Updated ExtendedHearingRequestResponse with judicial results.
    . Updated put hearing entity IT test.
    
    * PIC-2292
    . Added IT to verify the judicial result list order.
    . Added unit test
    
    * PIC-2292
    . Code Tidy up.
    
    * PIC-2292
    . Code Tidy up.
    
    * PIC-2292
    . Updated pact contract with judicial result changes
    
    * PIC-2292
    . Fixed failing test
    
    * PIC-2292
    . Pact test state name change
    
    * PIC-2292
    . Removed JudicialResultType and related changes.
    . Added JudicialResultTypeId field to JudicialResultEntity
    . Updated IT and unit tests.
    
    * PIC-2292
    . Tidy up
    
    * PIC-2292
    . Updated code to receive and return the same hearing event type string.
    . Updated test
    
    * PIC-2292
    . Fixed failing test
    
    * PIC-2292
    . Ignore null values

--- src/main/java/uk/gov/justice/probation/courtcaseservice/controller/model/OffenceRequestResponse.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/controller/model/OffenceRequestResponse.java
@@ -3,6 +3,7 @@ package uk.gov.justice.probation.courtcaseservice.controller.model;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +16,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OffenceRequestResponse {
     @NotBlank
     private final String offenceTitle;

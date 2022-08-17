25f3c4d68e55bcde8a596128df162a3390dce20d
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2022-08-17 12:01:04 +0100

    PIC-1 . Added required domain event model classes based on the schema.

---
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/model/event/AdditionalInformation.java
@@ -0,0 +1,15 @@
+package uk.gov.justice.probation.courtcaseservice.service.model.event;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class AdditionalInformation {
+    private final String referenceId;
+}

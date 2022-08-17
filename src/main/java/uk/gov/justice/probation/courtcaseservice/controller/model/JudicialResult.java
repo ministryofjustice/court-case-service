4586a0b999bb466b41274d8208fe5055841b9eda
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2022-08-17 08:50:32 +0100

    PIC-2292 . Ignore null values

--- src/main/java/uk/gov/justice/probation/courtcaseservice/controller/model/JudicialResult.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/controller/model/JudicialResult.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.probation.courtcaseservice.controller.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +13,7 @@ import lombok.With;
 @With
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class JudicialResult {
     private boolean isConvictedResult;
     private String label;

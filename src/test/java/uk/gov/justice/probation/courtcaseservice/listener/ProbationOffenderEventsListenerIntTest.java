eee298c71f2bee6cc23cf7362ca897b912e34b7c
rschallapalli <ravishankar.challapalli@digital.justice.gov.uk>
2022-09-26 15:10:30 +0100

    PIC-2379 update int tests to use database

--- src/test/java/uk/gov/justice/probation/courtcaseservice/listener/ProbationOffenderEventsListenerIntTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/listener/ProbationOffenderEventsListenerIntTest.java
@@ -6,42 +6,32 @@ import com.amazonaws.services.sns.model.PublishResult;
 import com.amazonaws.services.sqs.model.PurgeQueueRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import uk.gov.justice.probation.courtcaseservice.BaseIntTest;
-import uk.gov.justice.probation.courtcaseservice.jpa.entity.OffenderEntity;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.OffenderProbationStatus;
 import uk.gov.justice.probation.courtcaseservice.jpa.repository.OffenderRepository;
-import uk.gov.justice.probation.courtcaseservice.service.UserAgnosticOffenderService;
 import uk.gov.justice.probation.courtcaseservice.testUtil.OffenderEvent;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
 
+@Sql(scripts = "classpath:before-test.sql", config = @SqlConfig(transactionMode = ISOLATED))
+@Sql(scripts = "classpath:after-test.sql", config = @SqlConfig(transactionMode = ISOLATED), executionPhase = AFTER_TEST_METHOD)
 public class ProbationOffenderEventsListenerIntTest extends BaseIntTest {
 
-    @SpyBean
-    UserAgnosticOffenderService offenderService;
-
-    @SpyBean
+    @Autowired
     OffenderRepository offenderRepository;
 
     ObjectMapper objectMapper;
-    @Captor
-    ArgumentCaptor<String> crnArgumentCaptor;
-
-    @Captor
-    ArgumentCaptor<OffenderEntity> offenderEntityArgumentCaptor;
-
 
     @BeforeEach
     public void setUp() {
@@ -50,49 +40,30 @@ public class ProbationOffenderEventsListenerIntTest extends BaseIntTest {
     }
 
     @Test
-    public void shouldProcess_OffenderEventChangedMessage_AndUpdateOffenderProbationStatus() throws JsonProcessingException, InterruptedException {
-        var offenderEntity = OffenderEntity.builder()
-                .id(Long.valueOf(-1000001))
-                .cro("CROINT007")
-                .crn("X320741")
-                .pnc("PNCINT007")
-                .probationStatus(OffenderProbationStatus.CURRENT)
-                .previouslyKnownTerminationDate(LocalDate.of(2010, 1, 1))
-                .awaitingPsr(true)
-                .breach(true)
-                .preSentenceActivity(true)
-                .build();
+    public void shouldProcess_OffenderEventChangedMessage_AndUpdateOffenderProbationStatus() throws JsonProcessingException {
 
-        when(offenderRepository.findByCrn("X320741")).thenReturn(Optional.ofNullable(offenderEntity));
-
+        String crnForTest = "X781345";
         OffenderEvent sentencedEvent = OffenderEvent.builder()
-                .crn("X320741")
-                .offenderId(1L)
-                .nomsNumber("n123")
-                .sourceId(2L)
-                .eventDateTime(LocalDateTime.now())
-                .build();
+            .crn(crnForTest)
+            .offenderId(1L)
+            .nomsNumber("n123")
+            .sourceId(2L)
+            .eventDateTime(LocalDateTime.now())
+            .build();
 
         var result = publishOffenderEvent(sentencedEvent);
 
         assertThat(result.getSdkHttpMetadata().getHttpStatusCode()).isEqualTo(200);
         assertThat(result.getMessageId()).isNotNull();
 
-        Thread.sleep(2000);
-       // assertOffenderEventReceiverQueueHasProcessedMessages();
+        assertOffenderEventReceiverQueueHasProcessedMessages();
 
-        verify(offenderService).updateOffenderProbationStatus(crnArgumentCaptor.capture());
-        assertThat(crnArgumentCaptor.getValue()).isEqualTo("X320741");
+        var offenderEntityToUpdate = offenderRepository.findByCrn(crnForTest).get();
 
-        verify(offenderRepository).findByCrn(crnArgumentCaptor.capture());
-
-        verify(offenderRepository).save(offenderEntityArgumentCaptor.capture());
-
-        var offenderEntityToUpdate = offenderEntityArgumentCaptor.getValue();
-        assertThat(offenderEntityToUpdate.getCrn()).isEqualTo("X320741");
+        assertThat(offenderEntityToUpdate.getCrn()).isEqualTo(crnForTest);
         assertThat(offenderEntityToUpdate.getProbationStatus()).isEqualTo(OffenderProbationStatus.PREVIOUSLY_KNOWN);
         assertThat(offenderEntityToUpdate.getPreviouslyKnownTerminationDate()).isEqualTo(LocalDate.of(2010, 4, 5));
-        assertThat(offenderEntityToUpdate.isBreach()).isEqualTo(true);
+        assertThat(offenderEntityToUpdate.isBreach()).isEqualTo(false);
         assertThat(offenderEntityToUpdate.isPreSentenceActivity()).isEqualTo(true);
         assertThat(offenderEntityToUpdate.getAwaitingPsr()).isEqualTo(false);
     }
@@ -102,7 +73,7 @@ public class ProbationOffenderEventsListenerIntTest extends BaseIntTest {
         var messageAttribute = new MessageAttributeValue().withDataType("String").withStringValue("SENTENCE_CHANGED");
         var eventJson = objectMapper.writeValueAsString(offenderEvent);
         var offenderEventRequest = new PublishRequest(getOffenderEventTopic().getArn(), eventJson)
-                .withMessageAttributes(Collections.singletonMap("eventType", messageAttribute));
+            .withMessageAttributes(Collections.singletonMap("eventType", messageAttribute));
 
         return getOffenderEventTopic().getSnsClient().publish(offenderEventRequest);
 

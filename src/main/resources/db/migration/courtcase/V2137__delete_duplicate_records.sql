be4f42a8fb263b7586843f35e96361c24f3ad64b
John Evans <john.evans@digital.justice.gov.uk>
2022-12-06 17:31:12 +0000

    ⚡️PIC-2633: Improve migration performance

--- src/main/resources/db/migration/courtcase/V2137__delete_duplicate_records.sql
+++ src/main/resources/db/migration/courtcase/V2137__delete_duplicate_records.sql
@@ -3,8 +3,8 @@ BEGIN;
 ALTER TABLE judicial_result DROP CONSTRAINT fk_offence;
 ALTER TABLE judicial_result ADD CONSTRAINT fk_offence FOREIGN KEY (offence_id) REFERENCES offence (id) ON DELETE CASCADE;
 
-DELETE FROM hearing h WHERE h.id  NOT IN (SELECT max(id) FROM hearing h2 GROUP BY  h2.hearing_id);
+DELETE FROM hearing h USING (select id from hearing h2 except SELECT max(id) FROM hearing h2 GROUP BY  h2.hearing_id) as to_delete where h.id = to_delete.id;
 
-DELETE FROM defendant d WHERE d.defendant_id  NOT IN (SELECT hd.defendant_id FROM hearing_defendant hd);
+DELETE FROM defendant d using (select defendant_id from defendant d2 except SELECT hd.defendant_id FROM hearing_defendant hd) as to_delete where d.defendant_id = to_delete.defendant_id;
 
 COMMIT;

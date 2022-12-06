e131e23eb8c19db57dc91a6ff7228c430069cbf1
rschallapalli <ravishankar.challapalli@digital.justice.gov.uk>
2022-12-06 14:20:13 +0000

    PIC-2613 add cascade delet on judicial result

--- src/main/resources/db/migration/courtcase/V2137__delete_duplicate_records.sql
+++ src/main/resources/db/migration/courtcase/V2137__delete_duplicate_records.sql
@@ -1,5 +1,8 @@
 BEGIN;
 
+ALTER TABLE judicial_result DROP CONSTRAINT fk_offence;
+ALTER TABLE judicial_result ADD CONSTRAINT fk_offence FOREIGN KEY (offence_id) REFERENCES offence (id) ON DELETE CASCADE;
+
 DELETE FROM hearing h WHERE h.id  NOT IN (SELECT max(id) FROM hearing h2 GROUP BY  h2.hearing_id);
 
 DELETE FROM defendant d WHERE d.defendant_id  NOT IN (SELECT hd.defendant_id FROM hearing_defendant hd);

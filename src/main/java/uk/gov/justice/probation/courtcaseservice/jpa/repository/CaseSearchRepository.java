69ed160aa89264b44686371bea6a05709b21c5e2
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2022-12-08 10:12:20 +0000

    PIC-2605 . Added CaseSearchRepositoryIntTest

--- src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/repository/CaseSearchRepository.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/repository/CaseSearchRepository.java
@@ -1,10 +1,7 @@
 package uk.gov.justice.probation.courtcaseservice.jpa.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.HearingEntity;
 
--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/specification/CaseSearchSpecification.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/specification/CaseSearchSpecification.java
@@ -2,18 +2,12 @@ package uk.gov.justice.probation.courtcaseservice.service.specification;
 
 import org.springframework.data.jpa.domain.Specification;
 import uk.gov.justice.probation.courtcaseservice.controller.model.CaseSearchFilter;
-import uk.gov.justice.probation.courtcaseservice.controller.model.Defendant;
-import uk.gov.justice.probation.courtcaseservice.jpa.entity.DefendantEntity;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.HearingDayEntity;
-import uk.gov.justice.probation.courtcaseservice.jpa.entity.HearingDefendantEntity;
 import uk.gov.justice.probation.courtcaseservice.jpa.entity.HearingEntity;
-import uk.gov.justice.probation.courtcaseservice.jpa.entity.OffenderEntity;
 
-import javax.persistence.criteria.Join;
 import javax.persistence.criteria.ListJoin;
 import javax.persistence.criteria.Predicate;
 import java.time.LocalDate;
-import java.util.List;
 
 
 public class CaseSearchSpecification {
@@ -34,16 +28,4 @@ public class CaseSearchSpecification {
 
         });
     }
-
-    private static Specification<HearingEntity> buildProbationStatusFilter(List<String> probationStatusList) {
-        return ((root, query, criteriaBuilder) -> {
-            ListJoin<HearingEntity, HearingDefendantEntity> hearingEntityHearingDefendantEntityListJoin = root.joinList("hearingDefendants");
-            Join<HearingDefendantEntity, Defendant> hearingDefendantEntityDefendantJoin = hearingEntityHearingDefendantEntityListJoin.join("defendant");
-            Join<DefendantEntity, OffenderEntity> defendantEntityOffenderEntityJoin = hearingDefendantEntityDefendantJoin.join("offender");
-
-            return defendantEntityOffenderEntityJoin.get("probationStatus").in(probationStatusList);
-        });
-    }
-
-
 }

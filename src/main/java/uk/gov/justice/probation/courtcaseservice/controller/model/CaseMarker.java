f8b43a5f01942d2cb06848f4c86ae3f075e9e279
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-03-21 09:34:48 +0000

    PIC-2733
    . Addressed review comments.

--- src/main/java/uk/gov/justice/probation/courtcaseservice/controller/model/CaseMarker.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/controller/model/CaseMarker.java
@@ -1,16 +1,12 @@
 package uk.gov.justice.probation.courtcaseservice.controller.model;
 
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
-@Schema(description = "Verdict")
 @Data
 @Builder
 @AllArgsConstructor

b0f621512f6d99b776cb113885f2fbb715ed851f
rschallapalli <98027245+rschallapalli@users.noreply.github.com>
2023-06-30 13:50:30 +0100

    PIC-975 reorder delete scripts (#1088)

--- src/main/resources/db/migration/courtcase/V2158__remove_offender_match_groups_missing_ids.sql
+++ src/main/resources/db/migration/courtcase/V2158__remove_offender_match_groups_missing_ids.sql
@@ -6,6 +6,13 @@ BEGIN;
 
 --------- ISSUE 1 - Unexpected null values in case_id and defendant_id fields ------------
 
+    -- Cascade deletion from offender_match_group to offender_match
+ALTER TABLE offender_match DROP CONSTRAINT fk_offender_match_group;
+ALTER TABLE offender_match ADD CONSTRAINT fk_offender_match_group FOREIGN KEY(group_id) REFERENCES offender_match_group(id) ON DELETE CASCADE;
+
+-- Drop any records where we have neither CASE_ID nor DEFENDANT_ID as we can't do anything with this information
+DELETE FROM OFFENDER_MATCH_GROUP omg where omg.DEFENDANT_ID IS NULL and omg.CASE_ID IS NULL;
+
 -- Find and update any records where we have a defendant_id but not case_id
 UPDATE offender_match_group omg SET case_id = to_update.case_id FROM
     (select omg_inner.id as id, cc.case_id from court_case cc
@@ -15,15 +22,9 @@ UPDATE offender_match_group omg SET case_id = to_update.case_id FROM
         join offender_match_group omg_inner on text(omg_inner.defendant_id) = text(d.defendant_id)
         where omg_inner.case_id is null and omg_inner.defendant_id is not null ) as to_update where omg.id = to_update.id;
 
--- Drop any records where we have neither CASE_ID nor DEFENDANT_ID as we can't do anything with this information
-DELETE FROM OFFENDER_MATCH_GROUP omg where omg.DEFENDANT_ID IS NULL and omg.CASE_ID IS NULL;
 
 -- Add constraints to prevent the previous issue happening again
 ALTER TABLE OFFENDER_MATCH_GROUP ALTER COLUMN CASE_ID SET NOT NULL;
 ALTER TABLE OFFENDER_MATCH_GROUP ALTER COLUMN DEFENDANT_ID SET NOT NULL;
-
--- Cascade deletion from offender_match_group to offender_match
-ALTER TABLE offender_match DROP CONSTRAINT fk_offender_match_group;
-ALTER TABLE offender_match ADD CONSTRAINT fk_offender_match_group FOREIGN KEY(group_id) REFERENCES offender_match_group(id) ON DELETE CASCADE;
 
 END;

805591e72bb378bd705df1f735872dd03649abd5
Johny Farrar <me@johnyfarrar.co.uk>
2023-08-22 13:04:58 +0100

    Fix: Build and tests

--- src/test/kotlin/uk/gov/justice/probation/courtcaseservice/controller/CaseWorkflowControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/probation/courtcaseservice/controller/CaseWorkflowControllerIntTest.kt
@@ -31,7 +31,6 @@ internal class CaseWorkflowControllerIntTest: BaseIntTest() {
 
     companion object {
         val HEARING_ID = "1f93aa0a-7e46-4885-a1cb-f25a4be33a00"
-        val ASSIGNED_HEARING_ID = "96e3958e-3c21-11ee-be56-0242ac120002"
         val UNKNOWN_HEARING_ID = "111111-1111-1111-1111-111111111111"
         val hearingOutcomeRequest: String = "{ \"hearingOutcomeType\": \"ADJOURNED\" }"
         val hearingOutcomeAssignRequest: String = "{ \"assignedTo\": \"John Smith\" }"
@@ -94,9 +93,9 @@ internal class CaseWorkflowControllerIntTest: BaseIntTest() {
     }
 
     @Test
-    fun `given court code and outcome state IN_POGRESS return all outcomes for that court`() {
+    fun `given court code and outcome state IN_PROGRESS return all outcomes for that court`() {
 
-        var courtCode = "B33HU"
+        var courtCode = "B10JQ"
 
         val endpoint = UriComponentsBuilder.fromUri(URI("/courts/${courtCode}/hearing-outcomes"))
                 .queryParam("state", "IN_PROGRESS")
@@ -112,16 +111,16 @@ internal class CaseWorkflowControllerIntTest: BaseIntTest() {
                 .get(endpoint)
                 .then()
                 .statusCode(200)
-                .body("cases", hasSize<Any>(1))
+                .body("cases", hasSize<Any>(2))
                 .body("cases[0].hearingOutcomeType", equalTo("ADJOURNED"))
                 .body("cases[0].outcomeDate", equalTo("2023-04-24T09:09:09"))
-                .body("cases[0].hearingId", equalTo("ead45e2c-3c39-11ee-be56-0242ac120002"))
-                .body("cases[0].hearingDate", equalTo("2019-10-14"))
+                .body("cases[0].hearingId", equalTo("1f93aa0a-7e46-4885-a1cb-f25a4be33a00"))
+                .body("cases[0].hearingDate", equalTo("2019-11-14"))
                 .body("cases[0].defendantId", equalTo("40db17d6-04db-11ec-b2d8-0242ac130002"))
                 .body("cases[0].defendantName", equalTo("Mr Johnny BALL"))
                 .body("cases[0].probationStatus", equalTo("Current"))
                 .body("cases[0].assignedTo", equalTo("John Smith"))
-                .body("cases[0].assignedToUUid", equalTo("8f69def4-3c52-11ee-be56-0242ac120002"))
+                .body("cases[0].assignedToUuid", equalTo("8f69def4-3c52-11ee-be56-0242ac120002"))
     }
 
     @Test
@@ -156,11 +155,11 @@ internal class CaseWorkflowControllerIntTest: BaseIntTest() {
                 .accept(ContentType.JSON)
                 .body(hearingOutcomeAssignRequest)
                 .`when`()
-                .put("/hearing/{hearingId}/outcome/assign", ASSIGNED_HEARING_ID)
+                .put("/hearing/{hearingId}/outcome/assign", HEARING_ID)
                 .then()
                 .statusCode(200)
 
-        var hearing = hearingRepository.findFirstByHearingId(ASSIGNED_HEARING_ID).get();
+        var hearing = hearingRepository.findFirstByHearingId(HEARING_ID).get();
         assertThat(hearing.hearingOutcome.assignedTo).isEqualTo("John Smith")
         assertThat(hearing.hearingOutcome.assignedToUuid).isEqualTo("fb9a3bbf-360b-48d1-bdd6-b9292f9a0d81")
     }
--- src/test/resources/case-progress.sql
+++ src/test/resources/case-progress.sql
@@ -4,20 +4,12 @@ VALUES (-1000001, 'X320741', 'PNCINT007', 'CROINT007', 'CURRENT', '2010-01-01', 
 INSERT INTO courtcaseservicetest.hearing_outcome(id, outcome_type, outcome_date, state, created, created_by)
 VALUES (-1700020001, 'ADJOURNED', '2023-4-24 09:09:09', 'NEW', now(), 'case-progress.sql');
 
-INSERT INTO courtcaseservicetest.hearing_outcome(id, outcome_type, outcome_date, state, created, created_by)
-VALUES (-1700020002, 'ADJOURNED', '2023-4-24 09:09:09', 'NEW', now(), 'case-progress.sql');
-
-INSERT INTO courtcaseservicetest.hearing_outcome(id, outcome_type, outcome_date, state, created, created_by)
-VALUES (-1700020003, 'ADJOURNED', '2023-4-24 09:09:09', 'NEW', now(), 'case-progress.sql');
-
 INSERT INTO courtcaseservicetest.hearing_outcome(id, outcome_type, outcome_date, state, created, created_by, assigned_to, assigned_to_uuid)
-VALUES (-1700020004, 'ADJOURNED', '2023-4-24 09:09:09', 'IN_PROGRESS', now(), 'case-progress.sql', 'John Smith', '8f69def4-3c52-11ee-be56-0242ac120002');
+VALUES (-1700020002, 'ADJOURNED', '2023-4-24 09:09:09', 'IN_PROGRESS', now(), 'case-progress.sql', 'John Smith', '8f69def4-3c52-11ee-be56-0242ac120002');
 
 INSERT INTO courtcaseservicetest.court_case (id, case_id, created, source_type, urn) VALUES (-1700028600, '1f93aa0a-7e46-4885-a1cb-f25a4be33a00', now(), 'COMMON_PLATFORM', 'URN008');
 INSERT INTO courtcaseservicetest.hearing (id, fk_court_case_id, hearing_id, hearing_type, created, fk_hearing_outcome) VALUES (-1700028600, -1700028600, '1f93aa0a-7e46-4885-a1cb-f25a4be33a00', 'Sentence', TO_TIMESTAMP('2019-12-14 9:00:00', 'YYYY-MM-DD HH:MI:SS'), -1700020002);
 INSERT INTO courtcaseservicetest.hearing (id, fk_court_case_id, hearing_id, hearing_type, created, fk_hearing_outcome) VALUES (-1700028898, -1700028600, '2aa6f5e0-f842-4939-bc6a-01346abc09e7', 'Hearing', TO_TIMESTAMP('2019-10-14 9:00:00', 'YYYY-MM-DD HH:MI:SS'), -1700020001);
-INSERT INTO courtcaseservicetest.hearing (id, fk_court_case_id, hearing_id, hearing_type, created, fk_hearing_outcome) VALUES (-1700028899, -1700028600, '96e3958e-3c21-11ee-be56-0242ac120002', 'Hearing', TO_TIMESTAMP('2019-10-14 9:00:00', 'YYYY-MM-DD HH:MI:SS'), -1700020003);
-INSERT INTO courtcaseservicetest.hearing (id, fk_court_case_id, hearing_id, hearing_type, created, fk_hearing_outcome) VALUES (-1700028900, -1700028600, 'ead45e2c-3c39-11ee-be56-0242ac120002', 'Hearing', TO_TIMESTAMP('2019-10-14 9:00:00', 'YYYY-MM-DD HH:MI:SS'), -1700020004);
 
 INSERT INTO courtcaseservicetest.hearing_notes(id, hearing_id, note, "author", created, created_by, created_by_uuid)
 VALUES (-1700028800, '1f93aa0a-7e46-4885-a1cb-f25a4be33a00', 'Judge heard', 'Author One', now(), 'before-test.sql', 'fb9a3bbf-360b-48d1-bdd6-b9292f9a0d81');
@@ -37,8 +29,6 @@ INSERT INTO courtcaseservicetest.HEARING_DAY (id, fk_hearing_id, court_code, cou
 VALUES (-1000002, -1700028600, 'B10JQ', 1, '2019-12-14', '09:00');
 INSERT INTO courtcaseservicetest.HEARING_DAY (id, fk_hearing_id, court_code, court_room, hearing_day, hearing_time)
 VALUES (-1000001, -1700028898, 'B33HU', 2, '2019-10-14', '09:00');
-INSERT INTO courtcaseservicetest.HEARING_DAY (id, fk_hearing_id, court_code, court_room, hearing_day, hearing_time)
-VALUES (-1000003, -1700028900, 'B33HU', 1, '2019-10-14', '09:00');
 INSERT INTO courtcaseservicetest.DEFENDANT(id, DEFENDANT_ID, PERSON_ID, defendant_name, name, address, type, date_of_birth, crn, fk_offender_id, pnc, cro, sex, nationality_1, nationality_2, phone_number)
 VALUES (-1000000, '40db17d6-04db-11ec-b2d8-0242ac130002', 'c1a19768-4bad-11ed-bdc3-0242ac120002', 'Mr Johnny BALL', '{"title": "Mr", "surname": "BALL", "forename1": "Johnny", "forename2": "John", "forename3": "Jon"}', '{"line1": "27", "line2": "Elm Place", "postcode": "ad21 5dr", "line3": "Bangor", "line4": null, "line5": null}', 'PERSON', '1958-10-10', 'X320741', -1000001, 'A/1234560BA', '311462/13E', 'MALE', 'British', 'Polish', '{"home": "07000000013", "mobile": "07000000007", "work": "07000000015"}');
 
@@ -46,8 +36,6 @@ INSERT INTO courtcaseservicetest.HEARING_DEFENDANT(id, fk_hearing_id, DEFENDANT_
 VALUES (-1000000, -1700028600, '40db17d6-04db-11ec-b2d8-0242ac130002', -1000000);
 INSERT INTO courtcaseservicetest.HEARING_DEFENDANT(id, fk_hearing_id, DEFENDANT_ID, FK_DEFENDANT_ID)
 VALUES (-1000110, -1700028898, '40db17d6-04db-11ec-b2d8-0242ac130002', -1000000);
-INSERT INTO courtcaseservicetest.HEARING_DEFENDANT(id, fk_hearing_id, DEFENDANT_ID, FK_DEFENDANT_ID)
-VALUES (-1000210, -1700028900, '40db17d6-04db-11ec-b2d8-0242ac130002', -1000000);
 
 INSERT INTO courtcaseservicetest.OFFENCE (ID, FK_HEARING_DEFENDANT_ID, TITLE, SUMMARY, ACT, SEQUENCE, LIST_NO)
 VALUES (-1000000, -1000000, 'Theft from a shop', 'On 01/01/2015 at own, stole article, to the value of £987.00, belonging to person.', 'Contrary to section 1(1) and 7 of the Theft Act 1968.', 1, 10);

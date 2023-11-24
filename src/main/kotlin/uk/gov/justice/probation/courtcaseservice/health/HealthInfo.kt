aa7f730d6c11d1599d4f28244c9686ba17dec68d
cairnsp <101661307+cairnsp@users.noreply.github.com>
2023-11-24 15:01:58 +0000

    PIC-3361 Add HeathIndicator implementation (#1135)

---
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/health/HealthInfo.kt
@@ -0,0 +1,13 @@
+package uk.gov.justice.probation.courtcaseservice.health
+
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.boot.info.BuildProperties
+import org.springframework.stereotype.Component
+
+@Component
+class HealthInfo(buildProperties: BuildProperties) : HealthIndicator {
+    private val version: String = buildProperties.version
+
+    override fun health(): Health = Health.up().withDetail("version", version).build()
+}

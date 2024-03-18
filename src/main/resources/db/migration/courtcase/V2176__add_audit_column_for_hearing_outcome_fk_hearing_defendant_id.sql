34e2cb1c7db524d30ad7a38cc0328722ebe0fd48
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-03-18 15:20:30 +0000

    Specify schema

--- src/main/resources/db/migration/courtcase/V2176__add_audit_column_for_hearing_outcome_fk_hearing_defendant_id.sql
+++ src/main/resources/db/migration/courtcase/V2176__add_audit_column_for_hearing_outcome_fk_hearing_defendant_id.sql
@@ -1,7 +1,7 @@
 BEGIN;
-    ALTER TABLE if exists hearing_outcome_aud ADD COLUMN IF NOT EXISTS fk_hearing_defendant_id integer;
+    ALTER TABLE courtcaseservice.hearing_outcome_aud ADD COLUMN IF NOT EXISTS fk_hearing_defendant_id integer;
 
-    ALTER TABLE if exists  hearing_outcome_aud ADD COLUMN IF NOT EXISTS LEGACY boolean NOT NULL DEFAULT FALSE;
+    ALTER TABLE courtcaseservice.hearing_outcome_aud ADD COLUMN IF NOT EXISTS LEGACY boolean NOT NULL DEFAULT FALSE;
 
-    ALTER TABLE if exists hearing_outcome_aud ADD CONSTRAINT fk_hearing_defendant_hearing_outcome FOREIGN KEY (fk_hearing_defendant_id) REFERENCES hearing_defendant;
+    ALTER TABLE courtcaseservice.hearing_outcome_aud ADD CONSTRAINT fk_hearing_defendant_hearing_outcome FOREIGN KEY (fk_hearing_defendant_id) REFERENCES hearing_defendant;
 END;

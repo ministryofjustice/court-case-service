a2df5844b2900fc2cde6e772291412070b2de40b
Johny Farrar <me@johnyfarrar.co.uk>
2024-03-18 14:33:15 +0000

    Update V2176__add_audit_column_for_hearing_outcome_fk_hearing_defendant_id.sql

--- src/main/resources/db/migration/courtcase/V2176__add_audit_column_for_hearing_outcome_fk_hearing_defendant_id.sql
+++ src/main/resources/db/migration/courtcase/V2176__add_audit_column_for_hearing_outcome_fk_hearing_defendant_id.sql
@@ -4,6 +4,6 @@ ALTER TABLE if exists hearing_outcome_aud ADD COLUMN IF NOT EXISTS fk_hearing_de
 
 ALTER TABLE if exists hearing_outcome_aud ADD COLUMN IF NOT EXISTS LEGACY boolean NOT NULL DEFAULT FALSE;
 
-ALTER TABLE if exists hearing_outcome_aud ADD CONSTRAINT fk_hearing_defendant_hearing_outcome FOREIGN KEY (fk_hearing_defendant_id) REFERENCES hearing_defendant;
+ALTER TABLE hearing_outcome_aud ADD CONSTRAINT fk_hearing_defendant_hearing_outcome FOREIGN KEY (fk_hearing_defendant_id) REFERENCES hearing_defendant;
 
 END;

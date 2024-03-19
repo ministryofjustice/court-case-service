115d3c166a58aa60669bae9694a54b6c591c18d6
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-03-19 10:46:50 +0000

    Create Audit Tables for CircleCI job
    The CircleCI Job build_data_dictionary, creates a database.
    It does not create the Audit tables because SPRING_JPA_HIBERNATE_DDL_AUTO
    is not set to create, create-drop or update
    
    This means running migrations on an audit table e.g. hearing_outcome_aud,
    will result in an error because it was does not exist.

--- src/main/resources/db/migration/courtcase/V2176__add_audit_column_for_hearing_outcome_fk_hearing_defendant_id.sql
+++ src/main/resources/db/migration/courtcase/V2176__add_audit_column_for_hearing_outcome_fk_hearing_defendant_id.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
-ALTER TABLE if exists hearing_outcome_aud ADD COLUMN IF NOT EXISTS fk_hearing_defendant_id integer;
+ALTER TABLE hearing_outcome_aud ADD COLUMN IF NOT EXISTS fk_hearing_defendant_id integer;
 
-ALTER TABLE if exists hearing_outcome_aud ADD COLUMN IF NOT EXISTS LEGACY boolean NOT NULL DEFAULT FALSE;
+ALTER TABLE hearing_outcome_aud ADD COLUMN IF NOT EXISTS LEGACY boolean NOT NULL DEFAULT FALSE;
 
 ALTER TABLE hearing_outcome_aud ADD CONSTRAINT fk_hearing_defendant_hearing_outcome FOREIGN KEY (fk_hearing_defendant_id) REFERENCES hearing_defendant;
 

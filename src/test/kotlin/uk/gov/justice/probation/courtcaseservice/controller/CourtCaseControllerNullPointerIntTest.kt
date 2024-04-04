43b8a7b31ed8896043f24693af0bb89aeeabad09
Johny Farrar <me@johnyfarrar.co.uk>
2024-04-04 15:23:01 +0100

    ✅ Update and Pass tests

--- src/test/kotlin/uk/gov/justice/probation/courtcaseservice/controller/CourtCaseControllerNullPointerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/probation/courtcaseservice/controller/CourtCaseControllerNullPointerIntTest.kt
@@ -4,6 +4,8 @@ import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.hasSize
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.hasKey
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlConfig
@@ -37,6 +39,16 @@ internal class CourtCaseControllerNullPointerIntTest: BaseIntTest() {
             .statusCode(200)
             .body("caseId", equalTo("8a2f1cdc-b66e-4b1f-9fed-03089da8331b"))
             .body("hearings", hasSize<Any>(2))
+            .body("hearings[0].hearingId", equalTo("f57cf283-7a85-4c25-8412-4b73001e0b35"))
+            .body("hearings[0].hearingTypeLabel", equalTo("First hearing"))
+            .body("hearings[0].hearingOutcome.hearingOutcomeType", equalTo("NO_OUTCOME"))
+            .body("hearings[0].hearingOutcome.defendantName", equalTo("John SMITH"))
+            .body("hearings[0].hearingOutcome.state", equalTo("NEW"))
+            .body("hearings[0].hearingId", equalTo("c43c12e3-f8bc-4a07-bbce-63f9034ab360"))
+            .body("hearings[0].hearingTypeLabel", equalTo("First hearing"))
+            .body("hearings[0].hearingOutcome.hearingOutcomeType", equalTo("NO_OUTCOME"))
+            .body("hearings[0].hearingOutcome.defendantName", equalTo("John SMITH"))
+            .body("hearings[0].hearingOutcome.state", equalTo("NEW"))
             .log().all().extract().asString()
     }
 
@@ -56,6 +68,16 @@ internal class CourtCaseControllerNullPointerIntTest: BaseIntTest() {
             .statusCode(200)
             .body("caseId", equalTo("8a2f1cdc-b66e-4b1f-9fed-03089da8331b"))
             .body("hearings", hasSize<Any>(3))
-            .log().all().extract().asString()
+            .body("hearings[0].hearingId", equalTo("f57cf283-7a85-4c25-8412-4b73001e0b35"))
+            .body("hearings[0].hearingTypeLabel", equalTo("First hearing"))
+            .body("hearings[0].hearingOutcome.hearingOutcomeType", equalTo("NO_OUTCOME"))
+            .body("hearings[0].hearingOutcome.defendantName", equalTo("Dave JONES"))
+            .body("hearings[0].hearingOutcome.state", equalTo("NEW"))
+            .body("hearings[1].hearingId", equalTo("8007724a-ce9a-49f7-b8f5-3b3f177b7138"))
+            .body("hearings[1].hearingTypeLabel", equalTo("Bail Application"))
+            .body("hearings[1]", not(hasKey("hearingOutcome")))
+            .body("hearings[2].hearingId", equalTo("233efc02-9624-4ba7-abef-a439190d1361"))
+            .body("hearings[2].hearingTypeLabel", equalTo("Bail Application"))
+            .body("hearings[2]", not(hasKey("hearingOutcome")))
     }
 }

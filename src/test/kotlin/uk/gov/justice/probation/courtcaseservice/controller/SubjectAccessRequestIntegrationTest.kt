0455087ad49a75c1aacb4a0fde36ae8d890776f9
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-05-24 15:38:52 +0100

    Fix tests by providing an oauth role

--- src/test/kotlin/uk/gov/justice/probation/courtcaseservice/controller/SubjectAccessRequestIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/probation/courtcaseservice/controller/SubjectAccessRequestIntegrationTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.probation.courtcaseservice.controller
 
 import io.restassured.RestAssured
 import org.hamcrest.Matchers
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -85,6 +86,12 @@ class SubjectAccessRequestIntegrationTest : BaseIntTest() {
 
     @Nested
     inner class SARContent {
+
+      @BeforeEach
+      fun setAuthRoles(){
+        roles = listOf("ROLE_SAR_DATA_ACCESS")
+      }
+
       @Test
       fun `should return case comments, hearing outcomes and hearing notes if present for defendant`() {
 
@@ -173,7 +180,7 @@ class SubjectAccessRequestIntegrationTest : BaseIntTest() {
             "Accept",
             "application/json"
           )
-          .get("/subject-access-request?crn=B25829")
+          .get("/subject-access-request?crn=X25829&fromDate=2022-10-10")
           .then()
           .statusCode(200)
           .body("content.comments[0].comment", Matchers.equalTo("PSR in progress"))
@@ -183,13 +190,15 @@ class SubjectAccessRequestIntegrationTest : BaseIntTest() {
           .body("content.comments[0].lastUpdated", Matchers.equalTo("2024-04-08T09:45:55.597"))
           .body("content.comments[0].lastUpdatedBy", Matchers.equalTo("Last Updated Author"))
           .body("content.comments[0].caseNumber", Matchers.equalTo("1600028888"))
-          .body("content.comments[1].comment", Matchers.equalTo("PSR in progress"))
-          .body("content.comments[1].author", Matchers.equalTo("Author Three"))
-          .body("content.comments[1].created", Matchers.equalTo("2024-04-21T09:45:55.597"))
-          .body("content.comments[1].createdBy", Matchers.equalTo("before-test.sql"))
-          .body("content.comments[1].lastUpdated", Matchers.equalTo("2024-03-08T09:45:55.597"))
-          .body("content.comments[1].lastUpdatedBy", Matchers.equalTo("Last Updated Author3"))
-          .body("content.comments[1].caseNumber", Matchers.equalTo("1600028888"))
+          .body("content.hearingOutcomes[0].outcomeType", Matchers.equalTo("ADJOURNED"))
+          .body("content.hearingOutcomes[0].outcomeDate", Matchers.equalTo("2023-04-24T09:09:09"))
+          .body("content.hearingOutcomes[0].resultedDate", Matchers.equalTo("2023-04-25T09:09:09"))
+          .body("content.hearingOutcomes[0].state", Matchers.equalTo("IN_PROGRESS"))
+          .body("content.hearingOutcomes[0].assignedTo", Matchers.equalTo("John Smith"))
+          .body("content.hearingOutcomes[0].createdDate", Matchers.equalTo("2023-04-01T09:09:09"))
+          .body("content.hearingNotes[0].hearingId", Matchers.equalTo("fe657c3a-b674-4e17-8772-7281c99e4f9f"))
+          .body("content.hearingNotes[0].note", Matchers.equalTo("This is a test comment by the Prepare a case digital team."))
+          .body("content.hearingNotes[0].author", Matchers.equalTo("John Doe"))
       }
 
         @Test
@@ -212,10 +221,36 @@ class SubjectAccessRequestIntegrationTest : BaseIntTest() {
             .body("content.comments[0].lastUpdated", Matchers.equalTo("2024-04-08T09:45:55.597"))
             .body("content.comments[0].lastUpdatedBy", Matchers.equalTo("Last Updated Author"))
             .body("content.comments[0].caseNumber", Matchers.equalTo("1600028888"))
+            .body("content.hearingOutcomes", Matchers.empty<Any>())
+            .body("content.hearingNotes", Matchers.empty<Any>())
         }
 
+      @Test
+      fun `should return hearing notes and hearing outcomes if present for defendant from date`() {
+        RestAssured.given()
+          .auth()
+          .oauth2(getToken())
+          .`when`()
+          .header(
+            "Accept",
+            "application/json"
+          )
+          .get("/subject-access-request?crn=X25829&fromDate=2022-01-01")
+          .then()
+          .statusCode(200)
+          .body("content.hearingOutcomes[0].outcomeType", Matchers.equalTo("ADJOURNED"))
+          .body("content.hearingOutcomes[0].outcomeDate", Matchers.equalTo("2023-04-24T09:09:09"))
+          .body("content.hearingOutcomes[0].resultedDate", Matchers.equalTo("2023-04-25T09:09:09"))
+          .body("content.hearingOutcomes[0].state", Matchers.equalTo("IN_PROGRESS"))
+          .body("content.hearingOutcomes[0].assignedTo", Matchers.equalTo("John Smith"))
+          .body("content.hearingOutcomes[0].createdDate", Matchers.equalTo("2023-04-01T09:09:09"))
+          .body("content.hearingNotes[0].hearingId", Matchers.equalTo("fe657c3a-b674-4e17-8772-7281c99e4f9f"))
+          .body("content.hearingNotes[0].note", Matchers.equalTo("This is a test comment by the Prepare a case digital team."))
+          .body("content.hearingNotes[0].author", Matchers.equalTo("John Doe"))
+      }
+
         @Test
-        fun `should return case comments if present for defendant to date`() {
+        fun `should return case comments, hearing outcomes and hearing notes if present for defendant to date`() {
           RestAssured.given()
             .auth()
             .oauth2(getToken())
@@ -234,23 +269,34 @@ class SubjectAccessRequestIntegrationTest : BaseIntTest() {
             .body("content.comments[0].lastUpdated", Matchers.equalTo("2024-03-08T09:45:55.597"))
             .body("content.comments[0].lastUpdatedBy", Matchers.equalTo("Last Updated Author3"))
             .body("content.comments[0].caseNumber", Matchers.equalTo("1600028888"))
-            .body("content.comments[0].comment", Matchers.equalTo("PSR completed"))
-            .body("content.comments[0].author", Matchers.equalTo("Author Two"))
-            .body("content.comments[0].created", Matchers.equalTo("2024-05-22T09:45:55.597"))
-            .body("content.comments[0].createdBy", Matchers.equalTo("before-test.sql"))
-            .body("content.comments[0].lastUpdated", Matchers.equalTo("2024-04-09T09:45:55.597"))
-            .body("content.comments[0].lastUpdatedBy", Matchers.equalTo("Last Updated Author2"))
-            .body("content.comments[0].caseNumber", Matchers.equalTo(""))
-            .body("content.hearingOutcomes[0].hearingId", Matchers.equalTo("-198"))
             .body("content.hearingOutcomes[0].outcomeType", Matchers.equalTo("ADJOURNED"))
             .body("content.hearingOutcomes[0].outcomeDate", Matchers.equalTo("2023-04-24T09:09:09"))
             .body("content.hearingOutcomes[0].resultedDate", Matchers.equalTo("2023-04-25T09:09:09"))
             .body("content.hearingOutcomes[0].state", Matchers.equalTo("IN_PROGRESS"))
             .body("content.hearingOutcomes[0].assignedTo", Matchers.equalTo("John Smith"))
+            .body("content.hearingOutcomes[0].createdDate", Matchers.equalTo("2023-04-01T09:09:09"))
             .body("content.hearingNotes[0].hearingId", Matchers.equalTo("fe657c3a-b674-4e17-8772-7281c99e4f9f"))
             .body("content.hearingNotes[0].note", Matchers.equalTo("This is a test comment by the Prepare a case digital team."))
             .body("content.hearingNotes[0].author", Matchers.equalTo("John Doe"))
         }
+
+      @Test
+      fun `should return no case comments, hearing outcomes and hearing notes if not present for defendant to date`() {
+        RestAssured.given()
+          .auth()
+          .oauth2(getToken())
+          .`when`()
+          .header(
+            "Accept",
+            "application/json"
+          )
+          .get("/subject-access-request?crn=X25829&toDate=2021-04-25")
+          .then()
+          .statusCode(200)
+          .body("content.comments", Matchers.empty<Any>())
+          .body("content.hearingOutcomes", Matchers.empty<Any>())
+          .body("content.hearingNotes", Matchers.empty<Any>())
+      }
     }
   }
 }

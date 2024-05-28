4a42d184fbde5bae572a03d26797c42ecb30a68b
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-05-28 11:39:18 +0100

    Fix tests
    Allow hearing defendant id to be null when finding resources
    as the entityhelper hearing defendant has no id set.

--- src/main/kotlin/uk/gov/justice/probation/courtcaseservice/jpa/repository/HearingOutcomeRepository.kt
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/jpa/repository/HearingOutcomeRepository.kt
@@ -10,10 +10,10 @@ import java.util.*
 @Repository
 interface HearingOutcomeRepository: CrudRepository<HearingOutcomeEntity, Long> {
 
-    fun findByHearingDefendantId(hearingDefendantId: Long): List<HearingOutcomeEntity>
-    fun findAllByHearingDefendantIdAndCreatedBefore(hearingDefendantId: Long, toDateTime: LocalDateTime?): List<HearingOutcomeEntity>
+    fun findByHearingDefendantId(hearingDefendantId: Long?): List<HearingOutcomeEntity>
+    fun findAllByHearingDefendantIdAndCreatedBefore(hearingDefendantId: Long?, toDateTime: LocalDateTime?): List<HearingOutcomeEntity>
 
-    fun findAllByHearingDefendantIdAndCreatedAfter(hearingDefendantId: Long, fromDateTime: LocalDateTime?): List<HearingOutcomeEntity>
+    fun findAllByHearingDefendantIdAndCreatedAfter(hearingDefendantId: Long?, fromDateTime: LocalDateTime?): List<HearingOutcomeEntity>
 
-    fun findAllByHearingDefendantIdAndCreatedBetween(hearingDefendantId: Long, fromDateTime: LocalDateTime?, toDateTime: LocalDateTime?): List<HearingOutcomeEntity>
+    fun findAllByHearingDefendantIdAndCreatedBetween(hearingDefendantId: Long?, fromDateTime: LocalDateTime?, toDateTime: LocalDateTime?): List<HearingOutcomeEntity>
 }

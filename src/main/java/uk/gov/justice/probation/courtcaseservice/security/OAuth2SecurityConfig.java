82d028d61aa61ccd0b0e905f5f9ddd8aea8b7623
Jaspal Panesar <jaspal.panesar@digital.justice.gov.uk>
2024-06-03 11:37:41 +0100

    PIC-3848: Fix imports

--- src/main/java/uk/gov/justice/probation/courtcaseservice/security/OAuth2SecurityConfig.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/security/OAuth2SecurityConfig.java
@@ -3,21 +3,11 @@ package uk.gov.justice.probation.courtcaseservice.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import uk.gov.justice.hmpps.kotlin.auth.AuthAwareTokenConverter;
-
-import static org.springframework.security.oauth2.jose.jws.JwsAlgorithms.ES512;
-import static org.springframework.security.oauth2.jose.jws.JwsAlgorithms.RS512;
 
 @Configuration
 @EnableWebSecurity

a67ed7250e360886c5102c96cd7b764af41cf3c0
sameermoj <sameer.srivastava@digital.justice.gov.uk>
2024-11-07 16:01:41 +0000

    PIC-2840: Adding edited boolean field to HearingNoteResponse

--- src/test/java/uk/gov/justice/probation/courtcaseservice/controller/model/HearingNoteResponseTest.java
+++ src/test/java/uk/gov/justice/probation/courtcaseservice/controller/model/HearingNoteResponseTest.java
@@ -20,6 +20,7 @@ class HearingNoteResponseTest {
             .id(1234L)
             .createdByUuid("test-uuid")
             .created(now)
+            .lastUpdated(now)
             .draft(true)
             .legacy(true)
             .build();
@@ -37,6 +38,72 @@ class HearingNoteResponseTest {
                 .draft(true)
                 .legacy(true)
                 .build()
+        );
+    }
+
+    @Test
+    void givenHearingNoteEntity_updated_shouldMapToHearingNoteResponse() {
+        LocalDateTime createdDateTime = LocalDateTime.now().minusDays(1);
+        LocalDateTime updatedDateTime = LocalDateTime.now();
+        var hearingNoteEntity = HearingNoteEntity.builder()
+                .hearingId("test-hearing-id")
+                .author("Author One")
+                .note("Note one")
+                .id(1234L)
+                .createdByUuid("test-uuid")
+                .created(createdDateTime)
+                .lastUpdated(updatedDateTime)
+                .draft(true)
+                .legacy(true)
+                .build();
+
+        var hearingNoteResponse = HearingNoteResponse.of(hearingNoteEntity);
+
+        assertThat(hearingNoteResponse).isEqualTo(
+                HearingNoteResponse.builder()
+                        .hearingId("test-hearing-id")
+                        .author("Author One")
+                        .note("Note one")
+                        .noteId(1234L)
+                        .createdByUuid("test-uuid")
+                        .created(createdDateTime)
+                        .draft(true)
+                        .legacy(true)
+                        .edited(true)
+                        .build()
+        );
+    }
+
+
+    @Test
+    void givenHearingNoteEntity_not_updated_shouldMapToHearingNoteResponse() {
+        LocalDateTime createdDateTime = LocalDateTime.now().minusDays(1);
+        LocalDateTime updatedDateTime = LocalDateTime.now();
+        var hearingNoteEntity = HearingNoteEntity.builder()
+                .hearingId("test-hearing-id")
+                .author("Author One")
+                .note("Note one")
+                .id(1234L)
+                .createdByUuid("test-uuid")
+                .created(createdDateTime)
+                .draft(true)
+                .legacy(true)
+                .build();
+
+        var hearingNoteResponse = HearingNoteResponse.of(hearingNoteEntity);
+
+        assertThat(hearingNoteResponse).isEqualTo(
+                HearingNoteResponse.builder()
+                        .hearingId("test-hearing-id")
+                        .author("Author One")
+                        .note("Note one")
+                        .noteId(1234L)
+                        .createdByUuid("test-uuid")
+                        .created(createdDateTime)
+                        .draft(true)
+                        .legacy(true)
+                        .edited(false)
+                        .build()
         );
     }
 }

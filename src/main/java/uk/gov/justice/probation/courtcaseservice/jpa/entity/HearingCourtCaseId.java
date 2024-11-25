7a9b1d144a0f0e1448c5e9fdfb78fde5bd9705a5
Jaspal Panesar <jaspal.panesar@digital.justice.gov.uk>
2024-11-25 15:38:18 +0000

    PIC-4372: Use lombok

--- src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/HearingCourtCaseId.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/HearingCourtCaseId.java
@@ -1,18 +1,15 @@
 package uk.gov.justice.probation.courtcaseservice.jpa.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class HearingCourtCaseId implements Serializable {
     private String hearingId;
     private String caseId;
-
-    public HearingCourtCaseId() {}
-
-    public HearingCourtCaseId(String hearingId, String caseId) {
-        this.hearingId = hearingId;
-        this.caseId = caseId;
-    }
 }

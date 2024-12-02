260945eec6ad2db60ab9e5e697b3d8c536540474
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-12-02 17:08:49 +0000

    Implement feature flag

--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/DeleteHearingsService.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/DeleteHearingsService.java
@@ -1,13 +1,16 @@
 package uk.gov.justice.probation.courtcaseservice.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uk.gov.justice.probation.courtcaseservice.application.FeatureFlags;
 import uk.gov.justice.probation.courtcaseservice.jpa.dto.HearingCourtCaseDTO;
 import uk.gov.justice.probation.courtcaseservice.jpa.repository.DuplicateHearingRepository;
 import uk.gov.justice.probation.courtcaseservice.jpa.repository.HearingRepositoryFacade;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @Service
@@ -15,18 +18,31 @@ public class DeleteHearingsService {
 
     private final HearingRepositoryFacade hearingRepositoryFacade;
     private final DuplicateHearingRepository duplicateHearingRepository;
+    private final TelemetryService telemetryService;
 
-    public DeleteHearingsService(HearingRepositoryFacade hearingRepositoryFacade, DuplicateHearingRepository duplicateHearingRepository) {
+    @Autowired
+    private final FeatureFlags featureFlags;
+
+    public DeleteHearingsService(HearingRepositoryFacade hearingRepositoryFacade, DuplicateHearingRepository duplicateHearingRepository, TelemetryService telemetryService) {
         this.hearingRepositoryFacade = hearingRepositoryFacade;
         this.duplicateHearingRepository = duplicateHearingRepository;
+        this.telemetryService = telemetryService;
+        this.featureFlags = new FeatureFlags();
     }
 
     @Transactional
     public void deleteDuplicateHearings() {
         List<HearingCourtCaseDTO> oldestDuplicateHearings = duplicateHearingRepository.findOldestDuplicateHearings();
         oldestDuplicateHearings.forEach(hearingCourtCaseDTO -> {
-            hearingRepositoryFacade.deleteHearing(hearingCourtCaseDTO.getId());
-            log.info("Deleted hearing with id {}", hearingCourtCaseDTO.getId());
+            if (featureFlags.deleteHearing()) {
+                hearingRepositoryFacade.deleteHearing(hearingCourtCaseDTO.getId());
+                // it seems to not be an exact soft delete. it deletes child associations but only soft deletes the parent (the hearing) ?
+                log.info("Soft deleted duplicate hearing with id {}, hearing id {} and case id {}", hearingCourtCaseDTO.getId(), hearingCourtCaseDTO.getHearingId(), hearingCourtCaseDTO.getCaseId());
+                telemetryService.trackDeleteHearingEvent(TelemetryEventType.PIC_DELETE_HEARING, hearingCourtCaseDTO, featureFlags.deleteHearing());
+            } else {
+                log.info("DryRun enabled, soft deleting duplicate hearing with id {}, hearing id {} and case id {}", hearingCourtCaseDTO.getId(), hearingCourtCaseDTO.getHearingId(), hearingCourtCaseDTO.getCaseId());
+                telemetryService.trackDeleteHearingEvent(TelemetryEventType.PIC_DELETE_HEARING, hearingCourtCaseDTO, featureFlags.deleteHearing());
+            }
         });
     }
 }

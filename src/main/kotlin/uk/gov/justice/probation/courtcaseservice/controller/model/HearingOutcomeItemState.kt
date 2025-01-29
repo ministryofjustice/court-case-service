43c8a38af15df970cf5ee30b8b53c75f630b1ef8
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-01-29 13:37:38 +0000

    PIC-4471 remove jsonproperty annotation

--- src/main/kotlin/uk/gov/justice/probation/courtcaseservice/controller/model/HearingOutcomeItemState.kt
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/controller/model/HearingOutcomeItemState.kt
@@ -1,7 +1,5 @@
 package uk.gov.justice.probation.courtcaseservice.controller.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
-enum class HearingOutcomeItemState(@JsonProperty("hearingOutcomeState") val value: String) {
+enum class HearingOutcomeItemState(val value: String) {
     NEW("New"), IN_PROGRESS("In progress"), RESULTED("Resulted")
 }

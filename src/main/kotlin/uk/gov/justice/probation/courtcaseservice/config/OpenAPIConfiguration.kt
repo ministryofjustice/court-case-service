00600d9131c4693adc6290d3d9dacfb530d55351
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-01-30 12:29:59 +0000

    Revert "Update dependency uk.gov.justice.service.hmpps:hmpps-kotlin-spring-bo…" (#1326)
    This reverts commit 8e9e56de106c91f9855c49046ca4ef3bbb1ceba2.

--- src/main/kotlin/uk/gov/justice/probation/courtcaseservice/config/OpenAPIConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/config/OpenAPIConfiguration.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.License
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 

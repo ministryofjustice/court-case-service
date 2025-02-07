18aa07be423923107fed89f0ede04a2bf0a90ca0
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-02-07 17:03:42 +0000

    Remove unused import

--- src/main/java/uk/gov/justice/probation/courtcaseservice/application/healthchecks/NomisAuth.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/application/healthchecks/NomisAuth.java
@@ -2,7 +2,6 @@ package uk.gov.justice.probation.courtcaseservice.application.healthchecks;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.stereotype.Component;

34c2a6920f9f772dfadb901916655aba1188ab91
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-02-21 16:36:13 +0000

    Fix unique constraint

--- src/main/resources/db/migration/courtcase/V2192__add_unique_constraint_to_case_defendant.sql
+++ src/main/resources/db/migration/courtcase/V2192__add_unique_constraint_to_case_defendant.sql
@@ -1,4 +1,4 @@
 BEGIN;
-    alter table if exists case_defendant ADD UNIQUE (fk_court_case_id, fk_case_defendant_id);
-    alter table if exists case_defendant_aud ADD UNIQUE (fk_court_case_id, fk_case_defendant_id);
+    alter table if exists case_defendant ADD CONSTRAINT uniq_court_case_case_defendant UNIQUE (fk_court_case_id, fk_case_defendant_id);
+    alter table if exists case_defendant_aud ADD CONSTRAINT uniq_court_case_case_defendant UNIQUE (fk_court_case_id, fk_case_defendant_id);
 END;

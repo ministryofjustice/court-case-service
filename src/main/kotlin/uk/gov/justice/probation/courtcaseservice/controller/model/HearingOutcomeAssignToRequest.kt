6f261e3ba1e13dee2a2567a78ea1f946543c3642
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-03-11 15:07:22 +0000

    Remove duplicated dependencies
    Many of these dependencies are imported from gradle-spring-boot

--- src/main/kotlin/uk/gov/justice/probation/courtcaseservice/controller/model/HearingOutcomeAssignToRequest.kt
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/controller/model/HearingOutcomeAssignToRequest.kt
@@ -5,6 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Hearing outcome assigned to input model")
 data class HearingOutcomeAssignToRequest(
-    @JsonProperty("assignedTo")
-    val assignedTo: String
+  @JsonProperty("assignedTo")
+  val assignedTo: String,
 )

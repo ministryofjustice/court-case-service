38f1fba78975c978f4241dcea6e411b8cc0d5b88
Jaspal Panesar <jaspal.panesar@digital.justice.gov.uk>
2025-04-04 09:46:32 +0100

    PIC-4566: Adding extra status values to prevent potential 400 bad request (#1365)

--- src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/OffenderProbationStatus.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/jpa/entity/OffenderProbationStatus.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 public enum OffenderProbationStatus {
     CURRENT(DefendantProbationStatus.CURRENT.name()),
     PREVIOUSLY_KNOWN(DefendantProbationStatus.PREVIOUSLY_KNOWN.name()),
-    NOT_SENTENCED(DefendantProbationStatus.NOT_SENTENCED.name());
+    NOT_SENTENCED(DefendantProbationStatus.NOT_SENTENCED.name()),
+    UNCONFIRMED_NO_RECORD(DefendantProbationStatus.UNCONFIRMED_NO_RECORD.name()),
+    CONFIRMED_NO_RECORD(DefendantProbationStatus.CONFIRMED_NO_RECORD.name());
 
     private final String name;
 
@@ -32,6 +34,8 @@ public enum OffenderProbationStatus {
             case CURRENT -> DefendantProbationStatus.CURRENT;
             case PREVIOUSLY_KNOWN -> DefendantProbationStatus.PREVIOUSLY_KNOWN;
             case NOT_SENTENCED -> DefendantProbationStatus.NOT_SENTENCED;
+            case UNCONFIRMED_NO_RECORD -> DefendantProbationStatus.UNCONFIRMED_NO_RECORD;
+            case CONFIRMED_NO_RECORD -> DefendantProbationStatus.CONFIRMED_NO_RECORD;
         };
     }
 }

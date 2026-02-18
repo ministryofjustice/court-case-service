f6c25826f222e27ddcf0ffdd861dba4de944f977
Aron-v1 <aron.marriottsmith@justice.gov.uk>
2026-02-18 11:49:22 +0000

    PIC-5077 fix truncation in seeder

--- src/main/kotlin/uk/gov/justice/probation/courtcaseservice/database/seeders/framework/Seeder.kt
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/database/seeders/framework/Seeder.kt
@@ -21,25 +21,27 @@ abstract class Seeder(
 
   open fun clean() {
     // truncate DB tables in the correct order to avoid FK issues
+    // Omitting the schema prefix, so this will run against the current database (dev/test).
     entityManager.createNativeQuery(
-      "TRUNCATE courtcaseservicetest.offender_match_group CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.offender_match CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.hearing_day CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.hearing CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.hearing_defendant CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.defendant CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.offender CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.court_case CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.court CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.case_comments CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.hearing_outcome CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.hearing_notes CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.hearing_day_aud CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.offence_aud CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.hearing_defendant_aud CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.defendant_aud CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.hearing_aud CASCADE;\n" +
-        "TRUNCATE courtcaseservicetest.court_case_aud CASCADE;",
+        "TRUNCATE offender CASCADE;\n" +
+        "TRUNCATE offender_match_group CASCADE;\n" +
+        "TRUNCATE offender_match CASCADE;\n" +
+        "TRUNCATE hearing_day CASCADE;\n" +
+        "TRUNCATE hearing CASCADE;\n" +
+        "TRUNCATE hearing_defendant CASCADE;\n" +
+        "TRUNCATE case_comments CASCADE;\n" +
+        "TRUNCATE hearing_notes CASCADE;\n" +
+        "TRUNCATE verdict CASCADE;\n" +
+        "TRUNCATE court_case CASCADE;\n" +
+        "TRUNCATE judicial_result CASCADE;\n" +
+        "TRUNCATE offence CASCADE;\n" +
+        "TRUNCATE plea CASCADE;\n" +
+        "TRUNCATE defendant CASCADE;\n" +
+        "TRUNCATE hearing_outcome CASCADE;\n" +
+        "TRUNCATE revinfo CASCADE;\n" +
+        "TRUNCATE case_defendant CASCADE;\n" +
+        "TRUNCATE case_defendant_documents CASCADE;\n" +
+        "TRUNCATE case_defendant_documents_aud CASCADE;\n",
     ).executeUpdate()
   }
 }

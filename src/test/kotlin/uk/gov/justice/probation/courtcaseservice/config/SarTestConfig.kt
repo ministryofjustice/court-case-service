ace7ba931722007b0575b4ce9adf6744d731b688
michael.pitt <michael.pitt1@justice.gov.uk>
2026-05-21 11:54:00 +0100

    PIC-5402 - implemented tests for SAR

---
+++ src/test/kotlin/uk/gov/justice/probation/courtcaseservice/config/SarTestConfig.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.probation.courtcaseservice.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import uk.gov.justice.digital.hmpps.subjectaccessrequest.SarIntegrationTestHelper
+import uk.gov.justice.hmpps.test.kotlin.auth.JwtAuthorisationHelper
+
+@TestConfiguration
+open class SarTestConfig {
+
+  @Bean
+  open fun sarIntegrationTestHelper(
+    @Value("\${hmpps.sar.tests.expected-api-response.path:}") expectedApiResponsePath: String,
+    @Value("\${hmpps.sar.tests.expected-render-result.path:}") expectedRenderResultPath: String,
+    @Value("\${hmpps.sar.tests.attachments-expected:false}") attachmentsExpected: Boolean,
+    @Value("\${hmpps.sar.tests.expected-flyway-schema-version:0}") expectedFlywaySchemaVersion: String,
+    @Value("\${hmpps.sar.tests.expected-jpa-entity-schema.path:}") expectedJpaEntitySchemaPath: String,
+  ): SarIntegrationTestHelper = SarIntegrationTestHelper(
+    jwtAuthHelper = JwtAuthorisationHelper(),
+    expectedApiResponsePath = expectedApiResponsePath,
+    expectedRenderResultPath = expectedRenderResultPath,
+    attachmentsExpected = attachmentsExpected,
+    expectedFlywaySchemaVersion = expectedFlywaySchemaVersion,
+    expectedJpaEntitySchemaPath = expectedJpaEntitySchemaPath,
+  )
+}

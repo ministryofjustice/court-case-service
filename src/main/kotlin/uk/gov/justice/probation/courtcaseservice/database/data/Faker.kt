7b70a67ef46e5473e6374ca10bc554ea8c7207be
Aron Marriott-Smith <Aron.MarriottSmith@justice.gov.uk>
2026-08-07 19:00:22 +0100

    PIC-5077 add offence archetypes

--- src/main/kotlin/uk/gov/justice/probation/courtcaseservice/database/data/Faker.kt
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/database/data/Faker.kt
@@ -1,7 +1,11 @@
 package uk.gov.justice.probation.courtcaseservice.database.data
 
 import net.datafaker.Faker
+import uk.gov.justice.probation.courtcaseservice.database.data.offence.OffenceProvider
 import java.util.Locale
 
 // Preconfigure Faker with the UK locale so generated data is appropriate (e.g., names, addresses, phone numbers).
-class Faker : Faker(Locale.of("en-GB", "GB"))
+class Faker : Faker(Locale.of("en-GB", "GB")) {
+  // Usage: faker.offence().archetype()
+  fun offence() = OffenceProvider()
+}
---
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/database/data/offence/offenceArchetypes.kt
@@ -0,0 +1,35 @@
+package uk.gov.justice.probation.courtcaseservice.database.data.offence
+
+
+val offenceArchetypes = listOf(
+  OffenceArchetype(
+    title = "Robbery",
+    summary = "Acquisition, use or possession of criminal property",
+    act = "Section 329 of the Proceeds of Crime Act 2002",
+    offenceCode = "03803"
+  ),
+  OffenceArchetype(
+    title = "Theft offences",
+    summary = "Theft of pedal cycle",
+    act = "Section 12(5) of the Theft Act 1968",
+    offenceCode = "04400"
+  ),
+  OffenceArchetype(
+    title = "Fraud offences",
+    summary = "Refusal or failure to produce book makers permit on reques by a Constable.",
+    act = "Section 2(3) of the Betting, Gaming and Lotteries Act 1963",
+    offenceCode = "10629"
+  ),
+  OffenceArchetype(
+    title = "Summary motoring offences",
+    summary = "Drive on a horse drawn vehicle on a footpath",
+    act = "Section 72 of the Highways Act 1835",
+    offenceCode = "13501"
+  ),
+  OffenceArchetype(
+    title = "Criminal damage and arson",
+    summary = "Arson endangering life",
+    act = "Section 1(2) of the Criminal Damage Act 1971",
+    offenceCode = "05601"
+  ),
+)

b8e50d907f2624c8262c45223a34d48270ba33ff
Aron Marriott-Smith <Aron.MarriottSmith@justice.gov.uk>
2026-08-12 13:38:02 +0100

    PCFS-266 update court room assignment and time

--- src/main/kotlin/uk/gov/justice/probation/courtcaseservice/database/factories/HearingDayFactory.kt
+++ src/main/kotlin/uk/gov/justice/probation/courtcaseservice/database/factories/HearingDayFactory.kt
@@ -10,24 +10,31 @@ import java.time.LocalTime
 class HearingDayFactory(
   private val repository: HearingDayRepository,
   private var date: LocalDate = LocalDate.now(),
-  private var time: LocalTime = LocalTime.now(),
+  private var time: LocalTime? = null,
   private var courtCode: String = "B10JQ",
-  private var courtRoom: String = listOf("1", "2").random(),
+  private var courtRoom: String = "",
   private var hearing: HearingEntity? = null,
 ) {
 
-  fun count(count: Int = 1) = Factory(
-    newModel = {
-      HearingDayEntity.builder()
-        .id(null)
-        .day(date)
-        .time(time)
-        .courtCode(courtCode)
-        .courtRoom(courtRoom)
-        .hearing(hearing)
-        .build()
-    },
-    repository = repository,
-    count = count,
-  ).create()
+  fun count(count: Int = 1) : List<HearingDayEntity> {
+    courtRoom.ifEmpty { courtRoom = listOf("1", "2").random() }
+    if (time == null) {
+      time = LocalTime.of(listOf(9, 13).random(), 0)
+    }
+
+    return Factory(
+      newModel = {
+        HearingDayEntity.builder()
+          .id(null)
+          .day(date)
+          .time(time)
+          .courtCode(courtCode)
+          .courtRoom(courtRoom)
+          .hearing(hearing)
+          .build()
+      },
+      repository = repository,
+      count = count,
+    ).create()
+  }
 }

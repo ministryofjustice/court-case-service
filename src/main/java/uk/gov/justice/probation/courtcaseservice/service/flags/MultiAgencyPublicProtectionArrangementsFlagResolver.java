8c1ec9c95730510cb6a1874694c2fa4551bde1a9
michael.pitt <michael.pitt1@justice.gov.uk>
2026-09-24 11:35:12 +0100

    PCFS-271 - removed the hearing type condition for mappa as not required.

--- src/main/java/uk/gov/justice/probation/courtcaseservice/service/flags/MultiAgencyPublicProtectionArrangementsFlagResolver.java
+++ src/main/java/uk/gov/justice/probation/courtcaseservice/service/flags/MultiAgencyPublicProtectionArrangementsFlagResolver.java
@@ -12,13 +12,10 @@ import uk.gov.justice.probation.courtcaseservice.jpa.repository.OffenceMappaMapp
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
 public class MultiAgencyPublicProtectionArrangementsFlagResolver {
-
-    static final Set<String> ELIGIBLE_HEARING_TYPES = Set.of("sentence", "sentence (prosecution to attend)");
 
     private final OffenceMappaMappingRepository offenceMappaMappingRepository;
     private final FeatureFlags featureFlags;
@@ -33,50 +30,38 @@ public class MultiAgencyPublicProtectionArrangementsFlagResolver {
     }
 
     public Map<String, Boolean> buildMultiAgencyPublicProtectionArrangementsFlagsMap(List<Pair<CourtCaseEntity, DefendantEntity>> results) {
-        var allOffenceCodes = offenceFlagHelper.offenceCodesForResults(results, this::isEligibleHearingType);
+        var allOffenceCodes = offenceFlagHelper.offenceCodesForResults(results, hearing -> true);
         return offenceMappaMappingRepository.findByOffenceCodeIn(allOffenceCodes).stream()
             .collect(Collectors.toMap(OffenceMappaMappingEntity::getOffenceCode, OffenceMappaMappingEntity::isMultiAgencyPublicProtectionArrangementsFlag));
     }
 
     public Map<String, Boolean> buildMultiAgencyPublicProtectionArrangementsFlagsMapFromHearing(HearingEntity hearingEntity) {
-        var allOffenceCodes = offenceFlagHelper.offenceCodesForHearing(hearingEntity, this::isEligibleHearingType);
+        var allOffenceCodes = offenceFlagHelper.offenceCodesForHearing(hearingEntity, hearing -> true);
         return offenceMappaMappingRepository.findByOffenceCodeIn(allOffenceCodes).stream()
             .collect(Collectors.toMap(OffenceMappaMappingEntity::getOffenceCode, OffenceMappaMappingEntity::isMultiAgencyPublicProtectionArrangementsFlag));
     }
 
     public Boolean resolveMultiAgencyPublicProtectionArrangementsFlagFromHearing(HearingEntity hearingEntity, DefendantEntity defendant, Map<String, Boolean> multiAgencyPublicProtectionArrangementsFlagsByCode) {
         if (!featureFlags.enableMultiAgencyPublicProtectionArrangements()) return false;
-        var offenceCodes = offenceFlagHelper.offenceCodesForDefendant(hearingEntity, defendant.getDefendantId(), this::isEligibleHearingType);
+        var offenceCodes = offenceFlagHelper.offenceCodesForDefendant(hearingEntity, defendant.getDefendantId(), hearing -> true);
         return offenceFlagHelper.resolveFlag(offenceCodes, multiAgencyPublicProtectionArrangementsFlagsByCode);
     }
 
     public Map<String, Boolean> buildMultiAgencyPublicProtectionArrangementsFlagsMapFromDTOs(List<HearingDefendantDTO> defendants) {
-        var allOffenceCodes = offenceFlagHelper.offenceCodesForDTOs(defendants, this::isEligibleHearingType);
+        var allOffenceCodes = offenceFlagHelper.offenceCodesForDTOs(defendants, defendant -> true);
         return offenceMappaMappingRepository.findByOffenceCodeIn(allOffenceCodes).stream()
             .collect(Collectors.toMap(OffenceMappaMappingEntity::getOffenceCode, OffenceMappaMappingEntity::isMultiAgencyPublicProtectionArrangementsFlag));
     }
 
     public Boolean resolveMultiAgencyPublicProtectionArrangementsFlag(CourtCaseEntity courtCase, DefendantEntity defendant, Map<String, Boolean> multiAgencyPublicProtectionArrangementsFlagsByCode) {
         if (!featureFlags.enableMultiAgencyPublicProtectionArrangements()) return false;
-        var offenceCodes = offenceFlagHelper.offenceCodesForDefendant(courtCase, defendant.getDefendantId(), this::isEligibleHearingType);
+        var offenceCodes = offenceFlagHelper.offenceCodesForDefendant(courtCase, defendant.getDefendantId(), hearing -> true);
         return offenceFlagHelper.resolveFlag(offenceCodes, multiAgencyPublicProtectionArrangementsFlagsByCode);
     }
 
     public Boolean resolveMultiAgencyPublicProtectionArrangementsFlagFromDTO(HearingDefendantDTO defendant, Map<String, Boolean> multiAgencyPublicProtectionArrangementsFlagsByCode) {
         if (!featureFlags.enableMultiAgencyPublicProtectionArrangements()) return false;
-        var offenceCodes = offenceFlagHelper.offenceCodesForDTOs(List.of(defendant), this::isEligibleHearingType);
+        var offenceCodes = offenceFlagHelper.offenceCodesForDTOs(List.of(defendant), dto -> true);
         return offenceFlagHelper.resolveFlag(offenceCodes, multiAgencyPublicProtectionArrangementsFlagsByCode);
-    }
-
-    private boolean isEligibleHearingType(HearingEntity hearingEntity) {
-        return isEligibleHearingType(hearingEntity.getHearingType());
-    }
-
-    private boolean isEligibleHearingType(HearingDefendantDTO defendant) {
-        return isEligibleHearingType(defendant.getHearing() != null ? defendant.getHearing().getHearingType() : null);
-    }
-
-    private boolean isEligibleHearingType(String hearingType) {
-        return hearingType != null && ELIGIBLE_HEARING_TYPES.contains(hearingType.toLowerCase());
     }
 }
